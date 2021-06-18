6d3dcb76f61f2dc92e7371506dc0a1ccd804e090
naqvia <naqvia@email.chop.edu>
2021-06-18 14:53:44 -0400

    fixed barplot issues for RO3

--- analyses/psi_clustering/stacked_barplots.R
+++ analyses/psi_clustering/stacked_barplots.R
@@ -14,34 +14,30 @@ library("dplyr")
 library("vroom")
 library("ggplot2")
 
-##stacked barplot of cluster memberships
-cluster_mem_file = "/Users/naqvia/Desktop/AS-DMG/analyses/pan_cancer/results/cluster_hist_makeup.txt"
-cluster_mem      = read.delim(cluster_mem_file, sep = "\t",header=TRUE)
+file = "~/Desktop/cluster_hist_members.RO3.txt"
+cluster_mem      = read.delim(file, sep = "\t",header=TRUE)
 
-# Stacked
+
+##stacked barplot of cluster memberships
 cbbPalette <- c("#000000", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
 
-ggplot(cluster_mem, aes(fill=Hist, y=Counts, x=Cluster)) +
-  geom_bar(position="stack", stat="identity") + scale_fill_brewer(palette="Dark2") + theme_Publication()
 
-##stacked barplot of cluster memberships
-cluster_mem_file = "/Users/naqvia/Desktop/AS-DMG/analyses/psi_clustering/input/cluster_hist_members.txt"
+cluster_mem_file = "/Users/naqvia/Desktop/pbta-splicing/analyses/psi_clustering/input/cluster_hist_members.txt"
 cluster_mem      = read.delim(cluster_mem_file, sep = "\t",header=TRUE)
 
 ggplot(cluster_mem, aes(fill=short_histology, x=Cluster)) +
   geom_bar(stat="count", position="stack") + scale_fill_manual(values=c(cbbPalette)) + theme_Publication()
 
-
-
-
-cluster_mem_hgat_file = "/Users/naqvia/Desktop/AS-DMG/analyses/pan_cancer/results/cluster_hist_makeup.hgats.v3.txt"
+##stacked barplot of cluster memberships of HGATs
+cluster_mem_hgat_file = "/Users/naqvia/Desktop/pbta-splicing/analyses/psi_clustering/results/cluster_hist_members.hgats.txt"
 cluster_mem_hgat      = read.delim(cluster_mem_hgat_file, sep = "\t",header=TRUE)
 
 # Stacked
 cbbPalette_hgg <- c("#800000", "#8B0000", "#A52A2A", "#B22222", "#DC143C", "#FF0000", "#FF6347", "#FF7F50","#CD5C5C","#F08080","#E9967A","#FA8072");
 
 ggplot(cluster_mem_hgat, aes(fill=Subtype, x=Cluster)) +
-  geom_bar(stat="count", position="stack") + scale_fill_manual(values=c(cbbPalette_hgg)) + theme_Publication()
+  geom_bar(stat="count", position="stack")  + theme_Publication()
+#+ scale_fill_manual(values=c(cbbPalette_hgg))
 
 cluster_mem_hgat %>%
   ggplot() +
