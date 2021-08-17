326b4484239b12165acf1f232637bd3d25ab90d5
naqvia <naqvia@email.chop.edu>
2021-08-17 10:27:53 -0400

    updated script

--- analyses/splicing_index/SI_run.sh
+++ analyses/splicing_index/SI_run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "processing PSIs and generating tables"
-perl generate_splicing_index_tab.pl ../psi_clustering/input/pbta-histologies.RNA-Seq.initial.tsv ../../data/merge_rMATS_splicing.SE.single.tsv
+perl ../psi_clustering/input/pbta-histologies.RNA-Seq.initial.tsv ~/Desktop/AS-DMG/analyses/merge_rMATS/merge_rMATS_splicing.SE.single.tsv
 
 echo "plotting"
-Rscript splicing_index.R
+Rscript splicing_index_tumors.R
