5dcd418255ed853ec571b060c4143eb657edf6ed
Ammar S. Naqvi <naqvia@email.chop.edu>
2021-11-22 14:05:52 -0500

    Delete SI_run.sh

--- analyses/splicing_index/SI_run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-echo "processing PSIs and generating tables"
-perl generate_splicing_index_tab_using_tumors.pl ../psi_clustering/input/pbta-histologies.RNA-Seq.initial.tsv ../../data/merge_rMATS_splicing.SE.single.tsv
-
-echo "plotting"
-Rscript splicing_index_tumors.R
