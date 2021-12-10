b0affbd2aa4b5123ed1681b81d8a5665b1f24b0f
runjin326 <47674661+runjin326@users.noreply.github.com>
2021-12-09 22:28:12 -0500

    modifications made

--- analyses/kinase_activity_corr/kinase_activity_per_gene.R
+++ analyses/kinase_activity_corr/kinase_activity_per_gene.R
@@ -7,6 +7,9 @@ suppressPackageStartupMessages({
   library(readxl)
   library(optparse)
   library(Hmisc)
+  library(RColorBrewer)
+  library(magrittr)
+  library(corrplot)
 })
 
 #### Parse command line options ------------------------------------------------
@@ -27,27 +30,24 @@ pki_input_dir <- opt$pki_input_dir
 root_dir <- rprojroot::find_root(rprojroot::has_dir(".git"))
 analysis_dir <- file.path(root_dir, "analyses", "kinase_activity_corr")
 
-plots_dir <- file.path(analysis_dir, "plots", "individual_genes", "all")
-if(!dir.exists(plots_dir)){
-  dir.create(plots_dir, recursive=TRUE)
-}
-
-plots_sig_dir <- file.path(analysis_dir, "plots", "individual_genes", "significant")
+plots_sig_dir <- file.path(analysis_dir, "plots", "individual_genes", "significant_heatmap")
 if(!dir.exists(plots_sig_dir)){
   dir.create(plots_sig_dir, recursive=TRUE)
+}
+
+corr_plots_dir <- file.path(analysis_dir, "plots", "individual_genes", "corrplot")
+if(!dir.exists(corr_plots_dir)){
+  dir.create(corr_plots_dir, recursive=TRUE)
+}
+
+results_dir <- file.path(analysis_dir, "results")
+if(!dir.exists(results_dir)){
+  dir.create(results_dir, recursive=TRUE)
 }
 
 #### Read in files necessary for analyses
 # histology file 
 histology_df <- readr::read_tsv(opt$histology, guess_max=10000)
-
-# PSI group by CLK1 file
-psi_group_CLK1 <- readr::read_tsv(opt$group) %>%
-  dplyr::rename(Kids_First_Biospecimen_ID = samples,
-                PSI_group_by_CLK1 = Group) %>%
-  dplyr::select(-PSI)
-psi_group_CLK1$PSI_group_by_CLK1 <- factor(psi_group_CLK1$PSI_group_by_CLK1, 
-                                           levels = c("Low","Medium","High"))
 
 # kinase activity score
 kinase_activity <- readxl::read_excel(opt$kinase, sheet =3, skip=2) %>%
@@ -71,9 +71,7 @@ for (i in 1:length(file_list)){
   # match BS ID to sample ID
   histology_matched <- histology_df %>% 
     dplyr::filter(Kids_First_Biospecimen_ID %in% psi_of_interest$Kids_First_Biospecimen_ID) %>%
-    # add the CLK1 PSI group here
-    dplyr::left_join(psi_group_CLK1) %>% 
-    dplyr::select(Kids_First_Biospecimen_ID, sample_id, harmonized_diagnosis, short_histology, composition, RNA_library, PSI_group_by_CLK1)
+    dplyr::select(Kids_First_Biospecimen_ID, sample_id, short_histology, composition, RNA_library)
   
   # filter to only contain common samples
   common_samples <- histology_matched$sample_id[histology_matched$sample_id %in% colnames(kinase_activity)] %>%
@@ -106,43 +104,8 @@ for (i in 1:length(file_list)){
     tibble::column_to_rownames("sample_id") %>%
     dplyr::select(-c("RNA_library", "composition", "Kids_First_Biospecimen_ID"))
   
-  # order by PSI and then short histology
-  plot_matrix_1 <-histology_matched_filtered %>% 
-    arrange(PSI, short_histology)
   
-  # order the columns to plotting
-  kinase_activity_plot1 <-kinase_activity %>% 
-    # order the columns for plotting
-    dplyr::select(rownames(plot_matrix_1)) 
-  
-  pheatmap::pheatmap(as.matrix(kinase_activity_plot1),
-                     annotation_col = plot_matrix_1,
-                     cluster_rows=TRUE,
-                     cluster_cols=FALSE,
-                     width = 10,
-                     height = 8,
-                     show_colnames = F,
-                     filename = file.path(plots_dir, paste0(file_id, "_vs_kinase_by_psi_short.pdf")))
-  
-  # order by short histology and then PSI
-  plot_matrix_2 <-histology_matched_filtered %>% 
-    arrange(short_histology, PSI)
-  
-  # order the columns to plotting
-  kinase_activity_plot2 <-kinase_activity %>% 
-    # order the columns for plotting
-    dplyr::select(rownames(plot_matrix_2)) 
-  
-  pheatmap::pheatmap(as.matrix(kinase_activity_plot2),
-                     annotation_col = plot_matrix_2,
-                     cluster_rows=TRUE,
-                     cluster_cols=FALSE,
-                     width = 10,
-                     height = 8,
-                     show_colnames = F,
-                     filename = file.path(plots_dir, paste0(file_id, "_vs_kinase_by_short_psi.pdf")))
-  
-  ################### Additionally, plot out heatmaps with only sig. correlated genes
+  ################### Additionally, calculate correlation 
   histology_matched_filtered_calc <- histology_matched_filtered %>%
     tibble::rownames_to_column("sample_id") %>%
     dplyr::select(sample_id, PSI)
@@ -155,57 +118,86 @@ for (i in 1:length(file_list)){
     tibble::column_to_rownames("sample_id")
   
   # get the correlation results 
-  cor_sig_genes <- rcorr(as.matrix(kinase_activity_calc))$P %>%
+  res <- rcorr(as.matrix(kinase_activity_calc))
+  cor_sig_genes <- res$P %>%
     as.data.frame() %>%
     dplyr::select(PSI) %>%
     dplyr::filter(PSI < 0.05) %>%
     rownames() 
-  
-  if(length(cor_sig_genes) >=1){
-      
+           
+  ##### plot out heatmaps with only sig. correlated genes for SRRM1 only
+  if(length(cor_sig_genes) >=1 && grepl("SRRM1", file_id)){
+    
     # order by PSI and then short histology
-    plot_matrix_3 <-histology_matched_filtered %>% 
+    plot_matrix <-histology_matched_filtered %>% 
       arrange(PSI, short_histology)
     
+    plot_matrix$short_histology <- factor(plot_matrix$short_histology)
+    
+    # generate color list for heatmaps
+    qual_col_pals = brewer.pal.info[brewer.pal.info$category == 'qual',]
+    col_vector = unlist(mapply(brewer.pal, qual_col_pals$maxcolors, rownames(qual_col_pals)))
+    
+    # select n distinct colors short histology
+    n_short_hist <- plot_matrix %>% pull(short_histology) %>% unique() %>% length()
+    # generate a list of colors for each annotation 
+    set.seed(1015)
+    short_hist_color <- sample(col_vector, n_short_hist) %>% 
+      set_names(unique(plot_matrix$short_histology))
+    
+    anno_colors <- list(short_hist_color)
+    names(anno_colors) <- c("short_histology")
+    
     # order the columns to plotting
-    kinase_activity_plot3 <-kinase_activity %>% 
+    kinase_activity_plot <-kinase_activity %>% 
       tibble::rownames_to_column("proID") %>%
       dplyr::filter(proID %in% cor_sig_genes) %>%
       tibble::column_to_rownames("proID") %>%
       # order the columns for plotting
-      dplyr::select(rownames(plot_matrix_1)) 
+      dplyr::select(rownames(plot_matrix)) 
     
-    pheatmap::pheatmap(as.matrix(kinase_activity_plot3),
-                       annotation_col = plot_matrix_3,
+    pheatmap::pheatmap(as.matrix(kinase_activity_plot),
+                       annotation_col = plot_matrix,
                        cluster_rows=(length(cor_sig_genes) >=2),
                        cluster_cols=FALSE,
+                       annotation_colors = anno_colors,
                        width = 10,
                        height = 8,
                        show_colnames = F,
                        filename = file.path(plots_sig_dir, paste0(file_id, "_vs_kinase_by_psi_short.pdf")))
-    
-    # order by short histology and then PSI
-    plot_matrix_4 <-histology_matched_filtered %>% 
-      arrange(short_histology, PSI)
-    
-    # order the columns to plotting
-    kinase_activity_plot4 <-kinase_activity %>% 
-      tibble::rownames_to_column("proID") %>%
-      dplyr::filter(proID %in% cor_sig_genes) %>%
-      tibble::column_to_rownames("proID") %>%
-      # order the columns for plotting
-      dplyr::select(rownames(plot_matrix_4)) 
-    
-    pheatmap::pheatmap(as.matrix(kinase_activity_plot4),
-                       annotation_col = plot_matrix_4,
-                       cluster_rows=(length(cor_sig_genes) >=2),
-                       cluster_cols=FALSE,
-                       width = 10,
-                       height = 8,
-                       show_colnames = F,
-                       filename = file.path(plots_sig_dir, paste0(file_id, "_vs_kinase_by_short_psi.pdf")))
-    
   }
+  
+  ####### plot out correlation graph
+  pdf(file = file.path(corr_plots_dir, paste0(file_id, "_corrplot.pdf")))
+  p <- corrplot(as.matrix(res[["r"]]), 
+                type="lower", 
+                order="hclust",   
+                bg="transparent", 
+                sig.level = 0.05,
+                insig = "blank")
+  print(p)
+  dev.off()
+  
+  #### output the file with P and R value 
+  # get r score
+  r_scores <- res$r %>% 
+    as.data.frame() %>%
+    dplyr::select(PSI) %>% 
+    tibble::rownames_to_column("Kinase_name") %>%
+    dplyr::rename(r_scores = PSI)
+  
+  # get p score
+  p_scores <- res$P %>% 
+    as.data.frame() %>%
+    dplyr::select(PSI) %>% 
+    tibble::rownames_to_column("Kinase_name") %>%
+    dplyr::rename(p_scores = PSI)
+  
+  # combined r and p and add the name of the gene and write out results
+  left_join(r_scores, p_scores) %>% 
+    dplyr::mutate(RBP_gene_name = file_id) %>% 
+    readr::write_tsv(file.path(results_dir, paste0(file_id, "_correlation_results.tsv")))
 }
+
 
 
