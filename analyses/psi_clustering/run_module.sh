f06521c91aa455ae617ee797af7b8257e7c0041d
naqvia <naqvia@email.chop.edu>
2022-05-16 15:22:03 -0400

    modified run module

--- analyses/psi_clustering/run_module.sh
+++ analyses/psi_clustering/run_module.sh
@@ -11,8 +11,28 @@ script_directory="$(perl -e 'use File::Basename;
 cd "$script_directory" || exit
 
 echo $script_directory
-hist_file="$script_directory"/"$1"
-rmats_file="$script_directory"/"$2"
+# hist_file="$1"
+# rmats_file="$2"
+#
+# if [ -n "$1" ]; then
+#   echo "supplied hist file: $hist_file"
+#   #hist file: $hist_file
+# else
+#   #echo "default hist_file = ../../data/v19_plus_20210311_pnoc_rna.tsv"
+#   hist_file=../../data/v19_plus_20210311_pnoc_rna.tsv
+# fi
+#
+# if [ -n "$2" ]; then
+#   echo "supplied rmats file: rmats_file"
+#   #rmats_file=rmats_file
+# else
+#   #echo "default rmats_file = ../../data/merge_rMATS_splicing.SE.single.tsv"
+#   rmats_file="../../data/merge_rMATS_splicing.SE.single.tsv"
+# fi
+
+##default file paths for histology and rmats output
+hist_file="../../data/v19_plus_20210311_pnoc_rna.tsv"
+rmats_file="../../data/merge_rMATS_splicing.SE.single.tsv"
 
 echo "hist file:" $hist_file
 echo "rmats file:" $rmats_file
@@ -20,19 +40,8 @@ echo "rmats file:" $rmats_file
 ## create PSI matrix keeping only one patient sample for downstream clustering
 # ../../data/v19_plus_20210311_pnoc_rna.tsv ../../data/merge_rMATS_splicing.SE.single.tsv
 echo "create PSI matrix keeping only one patient sample for downstream clustering..."
-perl create_matrix_of_PSI_SE.pl $hist_file $rmats_file
+perl create_matrix_of_PSI_SE_gene.pl $hist_file $rmats_file
 
 ## run clustering method, save in plots folder
 echo "run clustering method, save in plots folder..."
 Rscript consensus_clustering.R results/pan_cancer_splicing_SE.txt
-
-## stacked barplots of cluster memberships, results in plots folder // not done yet
-echo "make stacked barplots of cluster memberships, results in plots folder..."
-Rscript stacked_barplots.R results/CC_groups.txt $hist_file
-
-## find cluster contributors, files saved in results folder
-echo "find cluster contributors, files saved in results folder..."
-
-##delete first column name in first line only
-cat results/pan_cancer_splicing_SE.txt | perl -pe 's/Splice_ID\t//g' > results/pan_cancer_splicing_SE.vtest_tmp.txt
-python vtest_calc.py -i results/pan_cancer_splicing_SE.vtest_tmp.txt -c results/CC_groups.txt -t Cluster -o plots/vtest_res. -v results/vtest_calc.tsv
