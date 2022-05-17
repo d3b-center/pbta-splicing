c41ccfd245262dcdf8879117d0f83e790c989ec4
naqvia <naqvia@email.chop.edu>
2022-05-17 16:54:50 -0400

    updated scripts

--- analyses/splicing_index/run_module.sh
+++ analyses/splicing_index/run_module.sh
@@ -11,12 +11,14 @@ script_directory="$(perl -e 'use File::Basename;
 cd "$script_directory" || exit
 
 echo $script_directory
-hist_file="$script_directory"/"$1" # ../psi_clustering/input/pbta-histologies.RNA-Seq.initial.tsv
-rmats_file="$script_directory"/"$2" #/../data/merge_rMATS_splicing.SE.single.tsv
+#hist_file="$script_directory"/"$1" # ../psi_clustering/input/pbta-histologies.RNA-Seq.initial.tsv
+#rmats_file="$script_directory"/"$2" #/../data/merge_rMATS_splicing.SE.single.tsv
+
+hist_file="../../data/pbta-histologies.RNA-Seq.initial.tsv"
+rmats_file="../../data/merge_rMATS_splicing.SE.single.tsv"
 
 echo "hist file:" $hist_file
 echo "rmats file:" $rmats_file
-
 
 echo "processing PSIs and generating tables"
 perl generate_splicing_index_tab_using_tumors.pl $hist_file $rmats_file
