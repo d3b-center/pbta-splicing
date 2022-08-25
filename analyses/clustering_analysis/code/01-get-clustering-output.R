48df85ed5a708af2eb9b18bdb933ef296533c793
komalsrathi <komal.rathi@gmail.com>
2022-08-25 14:43:40 -0400

    refactor scripts

--- analyses/clustering_analysis/code/01-get-clustering-output.R
+++ analyses/clustering_analysis/code/01-get-clustering-output.R
@@ -1,8 +1,10 @@
 # Author: Komal S. Rathi
-# Function: Get ccp clustering output for cluster of interest
+# Function: Get ccp clustering output for a specific combination of distance + algorithm + % variable genes 
+# for the splicing dataset, we chose km, euclidean at 0% (based on Ammar's manual inspection)
 
 # load libraries
 suppressPackageStartupMessages({
+  library(optparse)
   library(ConsensusClusterPlus)
   library(tidyverse)
   library(DGCA)
@@ -10,104 +12,53 @@ suppressPackageStartupMessages({
 })
 
 # source function to perform dip test
-source('util/perform_diptest.R')
-
-# get ccp output
-get_ccp_output <- function(input_mat, data_type = c("raw_counts", "non_expr"), var_genes, cluster_algorithm, cluster_distance, prefix){
-  
-  # set seed for reproducibility
-  set.seed(100)
-  
-  # remove rows where all values are 0
-  input_mat <- input_mat[rowSums(input_mat[])>0,]
-  
-  # remove columns and rows with sd == 0 
-  input_mat <- input_mat[apply(input_mat, MARGIN = 1,function(x) { sd(x) != 0} ),] 
-  input_mat <- input_mat[,apply(input_mat, MARGIN = 2,function(x) { sd(x) != 0} )] 
-  
-  # filter features for expression data
-  if(data_type == "raw_counts"){
-    
-    # filter by expression if var_prop > 0
-    if(var_genes > 0){
-      print("filter by expression")
-      input_mat <- DGCA::filterGenes(inputMat = input_mat, 
-                                     filterTypes = c("central", "dispersion"),
-                                     filterDispersionType = "cv", 
-                                     filterDispersionPercentile = 0.2,
-                                     sequential= TRUE)
-    }
-    
-    # transform raw counts using vst
-    print("transform using VST")
-    input_mat <- DESeq2::varianceStabilizingTransformation(round(as.matrix(input_mat)), blind = TRUE, fitType = "parametric")
-    input_mat <- as.data.frame(input_mat)
-  }
-  
-  # perform dip test if var_prop == 0 
-  if(var_genes == 0){
-    print("perform dip test")
-    input_mat <- perform_diptest(count_matrix = input_mat)
-  } else if(var_genes > 0){
-    # filter for variability if var_prop > 0
-    print("filter by % variability")
-    var_genes <- as.numeric(var_genes)
-    print(paste("subset to", var_genes, "%", "variable features"))
-    n <- round(var_genes/100*nrow(input_mat))
-    
-    # reduce the dataset to the most variable features, measured by median absolute deviation
-    mads <- apply(input_mat, 1, mad)
-    input_mat <- input_mat[rev(order(mads))[1:n],] 
-  }
-  
-  # output folder 
-  ccp_dir <- file.path('output/ccp_output')
-  dir.create(ccp_dir, showWarnings = F, recursive = T)
-  
-  # output prefix
-  prefix <- paste(prefix, cluster_algorithm, cluster_distance, var_genes, sep = "_")
-  
-  # run ConsensusClusterPlus
-  res.ccp <- ConsensusClusterPlus(d = as.matrix(input_mat),
-                                  clusterAlg = cluster_algorithm,
-                                  finalLinkage = "average",
-                                  distance = cluster_distance,
-                                  plot = "pdf",
-                                  reps = 50, 
-                                  maxK = 10, 
-                                  pItem = 0.8,
-                                  title = file.path(ccp_dir),
-                                  seed = 100)
-  
-  # rename consensus pdf output
-  old_name <- file.path(ccp_dir, 'consensus.pdf')
-  new_name <- file.path(ccp_dir, paste0(prefix, '_consensus.pdf'))
-  file.rename(from = old_name, to = new_name)
-  
-  # save entire consensus output 
-  saveRDS(res.ccp, file = file.path(ccp_dir, paste0(prefix, '_ccp.rds')))
-  
-  # save input matrix (filtered/normalized) for downstream analyses
-  saveRDS(input_mat, file = file.path(ccp_dir, paste0(prefix, '_matrix.rds')))
-}
+root_dir <- rprojroot::find_root(rprojroot::has_dir(".git"))
+analysis_dir <- file.path(root_dir, "analyses", "clustering_analysis")
+source(file.path(analysis_dir, 'util', 'perform_diptest.R'))
+source(file.path(analysis_dir, 'util', 'get_ccp_output.R'))
+         
+# parse parameters     
+option_list <- list(
+  make_option(c("--input_mat"), type = "character",
+              help = "Input matrix collapsed to unique features (.rds)"),
+  make_option(c("--data_type"), type = "character",
+              help = "data type of the input matrix (raw_counts or non_expr)"),
+  make_option(c("--var_genes"), type = "character",
+              help = "% variable genes"),
+  make_option(c("--cluster_algorithm"), type = "character",
+              help = "CCP clustering algorithm"),
+  make_option(c("--cluster_distance"), type = "character",
+              help = "CCP clustering distance"),
+  make_option(c("--prefix"), type = "character",
+              help = "prefix of output files")
+)
+opt <- parse_args(OptionParser(option_list = option_list))
+input_mat <- opt$input_mat
+input_mat <- readRDS(input_mat)
+data_type <- opt$data_type
+var_genes <- opt$var_genes
+var_genes <- as.numeric(var_genes)
+cluster_algorithm <- opt$cluster_algorithm
+cluster_distance <- opt$cluster_distance
+prefix <- opt$prefix
 
 # pbta splicing data
 # read input file
-input_mat <- readRDS('input/non_expr_pan_cancer_splice_subset.rds')
+# input_mat <- readRDS('input/non_expr_pan_cancer_splice_subset.rds')
 get_ccp_output(input_mat,
-               data_type = "non_expr", 
-               var_genes = 0, 
-               cluster_algorithm = "km", 
-               cluster_distance = "euclidean", 
-               prefix = "non_expr_pan_cancer_splice_subset")
+               data_type = data_type, 
+               var_genes = var_genes, 
+               cluster_algorithm = cluster_algorithm, 
+               cluster_distance = cluster_distance, 
+               prefix = prefix)
 
 # run function with same clustering parameters using pbta expression data
 # pbta expression data subsetted to samples in the splicing data
 # read input file
-input_mat <- readRDS('input/raw_counts_pbta_subset.rds')
-get_ccp_output(input_mat,
-               data_type = "raw_counts", 
-               var_genes = 0, 
-               cluster_algorithm = "km", 
-               cluster_distance = "euclidean", 
-               prefix = "raw_counts_pbta_subset")
+# input_mat <- readRDS('input/raw_counts_pbta_subset.rds')
+# get_ccp_output(input_mat,
+#                data_type = "raw_counts", 
+#                var_genes = 0, 
+#                cluster_algorithm = "km", 
+#                cluster_distance = "euclidean", 
+#                prefix = "raw_counts_pbta_subset")
