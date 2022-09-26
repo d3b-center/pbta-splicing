87c71bb3ea84ec3729e52b1a54b0baeefb7a764e
Ammar S. Naqvi <naqvia@email.chop.edu>
2022-09-26 09:52:02 -0400

    added theme code
    Added a figure folder and then sourced it. Will be used for all plots

--- analyses/splicing_index/01-splicing_index_tumors.R
+++ analyses/splicing_index/01-splicing_index_tumors.R
@@ -37,37 +37,10 @@ if(!dir.exists(plots_dir)){
 }
 
 ##theme for all plots
-theme_Publication <- function(base_size=15, base_family="Helvetica") {
-  library(grid)
-  library(ggthemes)
-  (theme_foundation(base_size=base_size, base_family=base_family)
-    + theme(plot.title = element_text(face = "bold",
-                                      size = rel(1.2), hjust = 0.5),
-            text = element_text(),
-            panel.background = element_rect(colour = NA),
-            plot.background = element_rect(colour = NA),
-            panel.border = element_rect(colour = NA),
-            axis.title = element_text(face = "bold",size = rel(1)),
-            axis.title.y = element_text(angle=90,vjust =2),
-            axis.title.x = element_text(vjust = -0.2),
-            axis.text = element_text(),
-            axis.line = element_line(colour="black"),
-            axis.ticks = element_line(),
-            panel.grid.major = element_line(colour="#f0f0f0"),
-            panel.grid.minor = element_blank(),
-            legend.key = element_rect(colour = NA),
-            legend.position = "right",
-            legend.direction = "vertical",
-            legend.key.size= unit(0.5, "cm"),
-            # legend.margin = unit(0.5, "cm"),
-            legend.margin = margin(5,5,5,5),
-            legend.title = element_text(face="bold"),
-            #plot.margin=unit(c(10,5,5,5),"mm"),
-            plot.margin=unit(c(10,5,5,10),"mm"),
-            strip.background=element_rect(colour="#f0f0f0",fill="#f0f0f0"),
-            strip.text = element_text(face="bold")
-    ))
-}
+# source function for theme for plots survival
+figures_dir <- file.path(root_dir, "figures")
+source(file.path(figures_dir, "theme_for_plots.R"))
+
 
 file <- "splicing_index.total.txt"
 splice_index <- readr::read_tsv(file.path(results_dir, file))
