2fd620313ab4d206b11c1b5629fa376f60fef2aa
Ammar S. Naqvi <naqvia@email.chop.edu>
2022-09-27 13:41:49 -0400

    modified code for plot theme

--- analyses/splicing_index/03-plot_diffExp_highlowSBI.R
+++ analyses/splicing_index/03-plot_diffExp_highlowSBI.R
@@ -16,38 +16,8 @@ library("DESeq2")
 # Get `magrittr` pipe
 `%>%` <- dplyr::`%>%`
 
-##theme for all plots
-theme_Publication <- function(base_size=15, base_family="Helvetica") {
-  library(grid)
-  library(ggthemes)
-  (theme_foundation(base_size=base_size, base_family=base_family)
-    + theme(plot.title = element_text(face = "bold",
-                                      size = rel(1.2), hjust = 0.5),
-            text = element_text(),
-            panel.background = element_rect(colour = NA),
-            plot.background = element_rect(colour = NA),
-            panel.border = element_rect(colour = NA),
-            axis.title = element_text(face = "bold",size = rel(1)),
-            axis.title.y = element_text(angle=90,vjust =2),
-            axis.title.x = element_text(vjust = -0.2),
-            axis.text = element_text(),
-            axis.line = element_line(colour="black"),
-            axis.ticks = element_line(),
-            panel.grid.major = element_line(colour="#f0f0f0"),
-            panel.grid.minor = element_blank(),
-            legend.key = element_rect(colour = NA),
-            legend.position = "right",
-            legend.direction = "vertical",
-            legend.key.size= unit(0.5, "cm"),
-            # legend.margin = unit(0.5, "cm"),
-            legend.margin = margin(5,5,5,5),
-            legend.title = element_text(face="bold"),
-            #plot.margin=unit(c(10,5,5,5),"mm"),
-            plot.margin=unit(c(10,5,5,10),"mm"),
-            strip.background=element_rect(colour="#f0f0f0",fill="#f0f0f0"),
-            strip.text = element_text(face="bold")
-    ))
-}
+
+
 
 ## set directories
 root_dir <- rprojroot::find_root(rprojroot::has_dir(".git"))
@@ -57,6 +27,11 @@ analysis_dir <- file.path(root_dir, "analyses", "splicing_index")
 input_dir   <- file.path(analysis_dir, "input")
 results_dir <- file.path(analysis_dir, "results")
 plots_dir   <- file.path(analysis_dir, "plots")
+
+## theme for all plots
+# source function for theme for plots survival
+figures_dir <- file.path(root_dir, "figures")
+source(file.path(figures_dir, "theme_for_plots.R"))
 
 ## output files for final plots
 file_volc_hgat_plot <- file.path(analysis_dir, "plots", "enhancedVolcano_hgat_sbi.png")
