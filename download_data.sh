7d69c6a272d9f3ff04740eea9c6359cf9c642d8b
Jo Lynne Rokita <jharenza@gmail.com>
2022-09-28 15:25:46 -0400

    Revert "splicing events across functional sites"

--- download_data.sh
+++ download_data.sh
@@ -1,65 +1,9 @@
+#!/usr/bin/bash
 
-set -e
-set -o pipefail
+# download files and put in data folder
 
-# Use the OpenPBTA bucket as the default.
-URL=${SPLICE_URL:-https://s3.amazonaws.com/d3b-openaccess-us-east-1-prd-pbta/pbta-splicing-2022}
-RELEASE=${SPLICE_RELEASE:-v1}
-PREVIOUS=${SPLICE_RELEASE:-NA}
-
-# Remove old symlinks in data
-find data -type l -delete
-
-# The md5sum file provides our single point of truth for which files are in a release.
-curl --create-dirs $URL/$RELEASE/md5sum.txt -o data/$RELEASE/md5sum.txt -z data/$RELEASE/md5sum.txt
-
-# Consider the filenames in the md5sum file and the release notes
-FILES=(`tr -s ' ' < data/$RELEASE/md5sum.txt | cut -d ' ' -f 2` release-notes.md)
-
-if [ -d "data/$PREVIOUS" ]
-then
-  # Find unchanged files
-  echo "Checking for unchanged files..."
-  cd data/$PREVIOUS
-  UNCHANGED=(`md5sum -c ../$RELEASE/md5sum.txt 2>/dev/null | grep OK |cut -d ':' -f 1  || true`)
-  echo $UNCHANGED
-  cd ../../
-
-  # Hard link unchanged files
-  for oldfile in "${UNCHANGED[@]}"
-  do
-    if [ ! -e "data/$RELEASE/$oldfile" ]
-    then
-      echo "Hard linking $oldfile"
-      ln data/$PREVIOUS/$oldfile data/$RELEASE/$oldfile
-    fi
-  done
-fi
-
-# Download the items in FILES if not already present
-for file in "${FILES[@]}"
-do
-  if [ ! -e "data/$RELEASE/$file" ]
-  then
-    echo "Downloading $file"
-    curl $URL/$RELEASE/$file -o data/$RELEASE/$file
-  fi
-done
-
-
-# Check the md5s for everything we downloaded except CHANGELOG.md
-cd data/$RELEASE
-echo "Checking MD5 hashes..."
-md5sum -c md5sum.txt
-cd ../../
-
-# Make symlinks in data/ to the files in the just downloaded release folder.
-for file in "${FILES[@]}"
-do
-  ln -sfn $RELEASE/$file data/$file
-done
-
-# make data directory unwritable in CI
-if [ "$RELEASE" == "testing" ]; then
-  chmod u-w data
-fi
+wget --no-check-certificate https://figshare.com/ndownloader/files/29171019 -O ./data/rMATS_merged.comparison.tsv
+wget --no-check-certificate https://figshare.com/ndownloader/files/30452106 -O ./data/rMATS_merged.single.SE.tsv
+wget --no-check-certificate https://figshare.com/ndownloader/files/35275717 -O ./data/pbta-histologies.RNA-Seq.initial.tsv
+wget --no-check-certificate https://figshare.com/ndownloader/files/35275588 -O ./data/v19_plus_20210311_pnoc_rna.tsv
+wget --no-check-certificate https://figshare.com/ndownloader/files/35361607 -O ./data/stranded_rsem_counts_tab.tsv
