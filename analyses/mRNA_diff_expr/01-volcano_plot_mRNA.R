eb11b791ae1f9eec4884203d1b6a3d8d114109e9
Ammar S. Naqvi <naqvia@email.chop.edu>
2022-10-19 09:48:21 -0400

    scripts and readme

---
+++ analyses/mRNA_diff_expr/01-volcano_plot_mRNA.R
@@ -0,0 +1,167 @@
+################################################################################
+# volcano_plot_mRNA.R
+# written by Ammar Naqvi
+#
+# usage: Rscript volcano_plot.R
+################################################################################
+
+
+suppressPackageStartupMessages({
+  library("EnhancedVolcano")
+  library("DESeq2")
+  library("sva")
+})
+
+
+# Get `magrittr` pipe
+`%>%` <- dplyr::`%>%`
+
+## set directories
+root_dir <- rprojroot::find_root(rprojroot::has_dir(".git"))
+data_dir <- file.path(root_dir, "data")
+analysis_dir <- file.path(root_dir, "analyses", "mRNA_diff_expr")
+
+input_dir   <- file.path(analysis_dir, "input")
+results_dir <- file.path(analysis_dir, "results")
+plots_dir   <- file.path(analysis_dir, "plots")
+
+
+## output files for final plots
+file_volc_hgat_SF_plot <- file.path(analysis_dir, "plots", "enhancedVolcano_ctrl_hgat_SFs.png")
+file_volc_H3K28_SF_plot <- file.path(analysis_dir, "plots", "enhancedVolcano_ctrl_h3k28_SFs.png")
+
+## get splicing index table with splicing factors filtered out
+file <- "/tpm_norm_vs_tumor.hgg.SFs.txt"
+gene_counts  <-  read.csv(paste0(input_dir, file), row.names = 1,  header=TRUE)
+
+batch <- c(rep(1, 61), rep(2, 137))
+filtered.counts <- gene_counts[rowSums(gene_counts>=2) >= 1, ]
+
+adjusted <- ComBat_seq(as.matrix(log2(filtered.counts+1)), batch=batch, group=NULL)
+corrected_mat <- 2^(adjusted)
+corrected_df <- as.data.frame(corrected_mat)
+corrected_df <- sapply( corrected_df, as.integer )
+
+## construct metadata
+design = data.frame(row.names = colnames(corrected_df),
+                    condition = c(rep("Healthy",10), rep("Tumor",188) ),
+                    libType   = c(rep("paired-end",198)))
+
+singleSamples = design$libType == "paired-end"
+new_countTable = (gene_counts[ , singleSamples ])
+condition = design$condition[ singleSamples ]
+
+
+
+cds = DESeqDataSetFromMatrix(countData=round(corrected_df),
+                             colData=design,
+                             design= ~ condition)
+
+
+cds = estimateSizeFactors( cds )
+cds = estimateDispersions(cds)
+
+## run deseq function to compute pvalues
+cds <- DESeq(cds)
+res <- results(cds)
+
+## label anything below <0.05 as signficant
+res$Significant <- ifelse(res$pvalue< 0.05, "P-val < 0.05", "Not Sig")
+
+
+
+EnhancedVolcano(res,
+                lab = gsub("ENSG[1234567890]+_", "",row.names(filtered.counts)), ## remove ensembleid portion
+                x = 'log2FoldChange',
+                y = 'pvalue',
+                ylim = c(0,33),
+                xlim = c(-3,3),
+                title = 'Healthy versus HGG',
+                pCutoff = 0.05,
+                FCcutoff = 1,
+                pointSize = ,
+                labSize = 3)
+
+
+
+ggsave(
+  file_volc_hgat_SF_plot,
+  plot = last_plot(),
+  device = NULL,
+  path = NULL,
+  scale = 1,
+  width =6.73,
+  height = 10.38,
+  units = "in",
+  dpi = 600,
+  limitsize = TRUE,
+  bg = NULL
+)
+
+
+## get count table index table for just H3K28
+file <- "/tpm_norm_vs_tumor.h3k28.SFs.v2.txt"
+gene_counts  <-  read.csv(paste0(input_dir, file), row.names = 1,  header=TRUE)
+batch <- c(rep(1, 45), rep(2, 38))
+
+filtered.counts <- gene_counts[rowSums(gene_counts>=2) >= 1, ]
+
+adjusted <- ComBat_seq(as.matrix(log2(filtered.counts+1)), batch=batch, group=NULL)
+corrected_mat <- 2^(adjusted)
+corrected_df <- as.data.frame(corrected_mat)
+corrected_df <- sapply( corrected_df, as.integer )
+
+
+## construct metadata
+design = data.frame(row.names = colnames(corrected_df),
+                    condition = c(rep("Healthy",10), rep("Tumor",73) ),
+                    libType   = c(rep("paired-end",83)))
+
+singleSamples = design$libType == "paired-end"
+new_countTable = (gene_counts[ , singleSamples ])
+condition = design$condition[ singleSamples ]
+
+cds = DESeqDataSetFromMatrix(countData=round(corrected_df),
+                             colData=design,
+                             design= ~ condition)
+
+cds = estimateSizeFactors( cds )
+sizeFactors( cds )
+
+cds = estimateDispersions(cds)
+
+## run deseq function to compute pvalues
+cds <- DESeq(cds)
+res <- results(cds)
+
+res$Significant <- ifelse(res$pvalue< 0.05, "P-val < 0.05", "Not Sig")
+
+EnhancedVolcano(res,
+                lab = gsub("ENSG[1234567890]+_", "",row.names(filtered.counts)), ## remove ensembleid portion
+                x = 'log2FoldChange',
+                y = 'pvalue',
+                ylim = c(0,27),
+                xlim = c(-3,3),
+                title = 'Healthy versus H3K28',
+                pCutoff = 0.05,
+                FCcutoff = 1,
+                pointSize = 2,
+                labSize = 3)
+
+
+
+ggsave(
+  file_volc_H3K28_SF_plot,
+  plot = last_plot(),
+  device = NULL,
+  path = NULL,
+  scale = 1,
+  width =6.73,
+  height = 10.38,
+  units = "in",
+  dpi = 600,
+  limitsize = TRUE,
+  bg = NULL
+)
+
+
---
+++ analyses/mRNA_diff_expr/02-SRSF11_plots.R
@@ -0,0 +1,73 @@
+################################################################################
+# SRSF11_plots.R
+# written by Ammar Naqvi
+#
+# usage: Rscript SRSF11_plot.R
+################################################################################
+
+
+suppressPackageStartupMessages({
+  library("sva")
+  library("EnhancedVolcano")
+  library("DESeq2")
+  library("reshape2")
+  library("tidyverse")
+})
+
+
+# Get `magrittr` pipe
+`%>%` <- dplyr::`%>%`
+
+## set directories
+root_dir <- rprojroot::find_root(rprojroot::has_dir(".git"))
+data_dir <- file.path(root_dir, "data")
+analysis_dir <- file.path(root_dir, "analyses", "mRNA_diff_expr")
+
+input_dir   <- file.path(analysis_dir, "input")
+results_dir <- file.path(analysis_dir, "results")
+plots_dir   <- file.path(analysis_dir, "plots")
+
+## output files for final plots
+file_SRSF11_plot <- file.path(analysis_dir, "plots", "SRSF11_hgat_stacked.png")
+file_SRSF11_corr_plot <- file.path(analysis_dir, "plots", "corr_rna_vs_psi_SRSF11.png")
+
+## get SRSF11 psi table
+file <- "/SRSF11_psi.txt"
+psi_tab  <-  read.delim(paste0(input_dir, file), sep = "\t", header=TRUE)
+
+## transform and melt data for ggplot
+melt_psi_tab <- melt(psi_tab, id=c("sample","id","histology"), variable.name =c("Type"))
+
+## stacked barplot 
+melt_psi_tab %>% 
+  #arrange(desc(value)) %>%
+  mutate(sample=fct_reorder(sample,value)) %>% 
+  ggplot(aes(x = sample,y = value, fill= Type ))+ 
+  geom_bar(position="stack", stat="identity") + scale_fill_manual(values=c("red",
+                                                                           "blue")) 
+# Save plot as PNG
+png(file_SRSF11_plot, 
+    res = 800, width = 16, height = 8, units = "in")
+last_plot()
+dev.off()
+
+## scatter plot of psi vs pct expression
+file <- "/SRSF11_splicing_vs_expr.txt"
+tab  <-  read.delim(paste0(input_dir, file), sep = "\t", header=TRUE)
+ggscatter(tab, x="Expr", y="dPSI", 
+          add = "reg.line", conf.int = TRUE, 
+          cor.coef = TRUE, cor.method = "pearson",
+          add.params = list(color = "blue",
+                            fill = "lightgray"),
+          ticks = TRUE,
+          xticks.by = .1, yticks.by = .1,
+          xlab = "Expr", ylab = "dPSI") + theme_Publication()
+
+
+# Save plot as PNG
+png(file_SRSF11_corr_plot, 
+    res = 800, width = 4, height = 4, units = "in")
+last_plot()
+dev.off()
+
+
