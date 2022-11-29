3d0b966e15d19f59c7aac551e781cf5e002f24ec
Ammar S. Naqvi <naqvia@email.chop.edu>
2022-11-29 16:28:57 -0500

    Update 06-plot_psi_vs_expr.R

--- analyses/CLK1_specific/06-plot_psi_vs_expr.R
+++ analyses/CLK1_specific/06-plot_psi_vs_expr.R
@@ -5,13 +5,15 @@
 # usage: Rscript 05-plot_psi_vs_expr.R
 ################################################################################
 
-## libraries needed
 suppressPackageStartupMessages({
   library("ggplot2")
   library("dplyr")
   library("tidyverse")
   
 })
+
+# Get `magrittr` pipe
+`%>%` <- dplyr::`%>%`
 
 ## set directories
 root_dir <- rprojroot::find_root(rprojroot::has_dir(".git"))
@@ -21,9 +23,6 @@ analysis_dir <- file.path(root_dir, "analyses", "CLK1_specific")
 input_dir   <- file.path(analysis_dir, "input")
 results_dir <- file.path(analysis_dir, "results")
 plots_dir   <- file.path(analysis_dir, "plots")
-
-# Get `magrittr` pipe
-`%>%` <- dplyr::`%>%`
 
 ## function to get stats from to print on box plot
 get_box_stats <- function(y, upper_limit = max(corr_df$V3) * 1.15) {
@@ -44,17 +43,12 @@ corr_expr_spl_file = file.path(results_dir, "expr_vs_psi_corr_res.txt")
 corr_df=read.delim(corr_expr_spl_file,header=FALSE,sep = " ", row.names = NULL)
 
 ## make corr plot
-p <- ggplot(corr_df, aes(x = V1,y=V3,fill = "red"), legend = FALSE) + 
-   coord_flip() + geom_dotplot(binwidth = .1, binaxis='y', stackdir='centerwhole',binpositions = "all",dotsize=.1) +
-  theme_Publication() + 
-  ggtitle("Exon splicing vs mRNA expression") +
-  xlab("Gene") + ylab("Correlation Coeffecient") 
-p 
-
 p <- ggplot(corr_df, aes(V3, V2))  + stat_boxplot(fill = "lightgreen", colour = "black", outlier.colour = "red") + 
-                                     xlab("correlation coeff") + geom_jitter(width = 0.3) + ylab("") + xlim(-1,1) + 
-                                     ggtitle(" Exon splicing vs gene expression") +  stat_summary(fun.data = get_box_stats, geom = "text", hjust = .4, vjust = .2) +
-                                      theme_Publication()  
+  xlab("correlation coeff") + geom_jitter(width = 0.3) + ylab("") + xlim(-1,1) + 
+  ggtitle(" Exon splicing vs gene expression") +  stat_summary(fun.data = get_box_stats, geom = "text", hjust = .4, vjust = .2) +
+  theme_Publication()  
+
+
 
 ## save plot
 ggsave(file_corr_plot, width = 15, height = 5)
