2c42c083af53a747c58735062808042eaabb7fc0
jharenza <jolynnerokita@d3b.center>
2022-11-30 17:25:32 -0500

    fix pCutoff, bold/update axis and legend labels

--- analyses/CLK1_specific/02-plot_CLK1_high_v_low_Exon4_volcano.R
+++ analyses/CLK1_specific/02-plot_CLK1_high_v_low_Exon4_volcano.R
@@ -59,15 +59,18 @@ volc_plot <- EnhancedVolcano(res,
                 ylim = c(0,8),
                 xlim = c(-4,4),
                 title = 'Tumors with low vs. high CLK1 exon 4 inclusion',
-                pCutoff = 0.005,
+                pCutoff = 0.05,
                 subtitle = NULL,
                 FCcutoff = 2,
                 pointSize = 1,
                 labSize = 3,
                 typeConnectors = "closed",
-                #drawConnectors = TRUE,
                 widthConnectors = 0.15,
-                colConnectors = 'black')
+                colConnectors = 'black',
+                caption = NULL,
+                ylab = bquote(bold(~-log[10] ~ p[adj])),
+                xlab = bquote(bold(~log[2] ~ "Fold Change")),
+                legendLabels = c("NS", expression(log[2] ~ FC), "adj p-value", expression("adj p-value and" ~ log[2] ~ FC)))
 
 plot_file = file.path(plots_dir,"high_v_low_clk1_exon4_inclusion.volano.pdf") 
 
@@ -76,3 +79,10 @@ pdf(plot_file,
     width = 10, height = 10)
 volc_plot
 dev.off()
+
+
+
+
+
+
+
