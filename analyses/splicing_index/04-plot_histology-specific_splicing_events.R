6390b8721c160d20e5b6772389ed043641dd4ebf
Ammar S. Naqvi <naqvia@email.chop.edu>
2023-02-01 15:24:30 -0500

    modified upsetR perl and R scripts to make one merged file to plot

--- analyses/splicing_index/04-plot_histology-specific_splicing_events.R
+++ analyses/splicing_index/04-plot_histology-specific_splicing_events.R
@@ -14,6 +14,9 @@ suppressPackageStartupMessages({
   library("viridis")
 })
 
+#Get `magrittr` pipe
+`%>%` <- dplyr::`%>%`
+
 ##set up directories
 root_dir <- rprojroot::find_root(rprojroot::has_dir(".git"))
 data_dir <- file.path(root_dir, "data")
@@ -31,26 +34,31 @@ upsetR_ei_plot_file <- file.path(analysis_dir, "plots", "upsetR_histology-specif
 upsetR_tiff_es_plot_file     <- file.path(analysis_dir, "plots", "upsetR_histology-specific.es.tiff")
 upsetR_tiff_ei_plot_file <- file.path(analysis_dir, "plots", "upsetR_histology-specific.ei.tiff")
 
-## exon skipping
-ATRT_events = read.delim(paste0(results_dir, "/splicing_events.hist-labeled_list.thr10freq.pos.ATRT.txt"), sep = "\t", header=FALSE)
-CPG_events = read.delim(paste0(results_dir, "/splicing_events.hist-labeled_list.thr10freq.pos.CPG.txt"), sep = "\t", header=FALSE)
-GNG_events = read.delim(paste0(results_dir, "/splicing_events.hist-labeled_list.thr10freq.pos.GNG.txt"), sep = "\t", header=FALSE)
-EPN_events = read.delim(paste0(results_dir, "/splicing_events.hist-labeled_list.thr10freq.pos.EPN.txt"), sep = "\t", header=FALSE)
-HGG_events = read.delim(paste0(results_dir, "/splicing_events.hist-labeled_list.thr10freq.pos.HGG.txt"), sep = "\t", header=FALSE)
-MB_events = read.delim(paste0(results_dir, "/splicing_events.hist-labeled_list.thr10freq.pos.MB.txt"), sep = "\t", header=FALSE)
-LGG_events = read.delim(paste0(results_dir, "/splicing_events.hist-labeled_list.thr10freq.pos.LGG.txt"), sep = "\t", header=FALSE)
+splice_event_df = vroom(paste0(results_dir,"/","splicing_events.hist-labeled_list.thr10freq.txt"), delim="\t", trim_ws = TRUE, col_names = TRUE)
 
-listInput <- list("ATRT" =ATRT_events$V1, 
-                  "CPG" =CPG_events$V1,
-                  "GNG" =GNG_events$V1,
-                  "EPN" =EPN_events$V1,
-                  "HGG" =HGG_events$V1, 
-                  "MB" = MB_events$V1,
-                  "LGG"=LGG_events$V1)
+## select and create list for exon skipping only events
+splice_event_df_ATRT <- splice_event_df %>% filter(histology=='ATRT', type=='skipping') %>% select(splicing_event)
+splice_event_df_CPG <- splice_event_df %>% filter(histology=='CPG', type=='skipping') %>% select(splicing_event)
+splice_event_df_GNG <- splice_event_df %>% filter(histology=='GNG', type=='skipping') %>% select(splicing_event)
+splice_event_df_EPN <- splice_event_df %>% filter(histology=='EPN', type=='skipping') %>% select(splicing_event)
+splice_event_df_HGG <- splice_event_df %>% filter(histology=='HGG', type=='skipping') %>% select(splicing_event)
+splice_event_df_MB <- splice_event_df %>% filter(histology=='MB', type=='skipping') %>% select(splicing_event)
+splice_event_df_LGG <- splice_event_df %>% filter(histology=='LGG', type=='skipping') %>% select(splicing_event)
 
-es_events <- upset(fromList(listInput), 
-      mainbar.y.label = "", sets=c("ATRT","CPG","GNG","EPN","HGG","MB","LGG"), sets.x.label = "Histology", order.by = "freq",
-      mb.ratio = c(0.5,0.50), text.scale = c(1.3, 1.3, 1.3, 1.3, 2, 1.4),point.size = 2, line.size = 1.5, nsets = 7)
+list_for_skipping_upsetR <- list("ATRT"=splice_event_df_ATRT$splicing_event,
+                                  "CPG"=splice_event_df_CPG$splicing_event,
+                                 "GNG"=splice_event_df_GNG$splicing_event,
+                                 "EPN"=splice_event_df_EPN$splicing_event,
+                                 "HGG"=splice_event_df_HGG$splicing_event,
+                                 "MB"=splice_event_df_MB$splicing_event,
+                                 "LGG"=splice_event_df_LGG$splicing_event)
+                                 
+                                 
+
+es_events <- upset(fromList(list_for_skipping_upsetR), 
+                   mainbar.y.label = "", sets=c("ATRT","CPG","GNG","EPN","HGG","MB","LGG"), sets.x.label = "Histology", order.by = "freq",
+                   mb.ratio = c(0.5,0.50), text.scale = c(1.3, 1.3, 1.3, 1.3, 2, 1.4),point.size = 2, line.size = 1.5, nsets = 7)
+
 
 # Save plot as PDF
 pdf(upsetR_es_plot_file, width = 16, height = 8)
@@ -62,27 +70,27 @@ tiff(upsetR_tiff_es_plot_file, height = 1200, width = 2400, res = 300)
 print(es_events)
 dev.off()
 
-## exon inclusion
-ATRT_events = read.delim(paste0(results_dir, "/splicing_events.hist-labeled_list.thr10freq.neg.ATRT.txt"), sep = "\t", header=FALSE)
-CPG_events = read.delim(paste0(results_dir, "/splicing_events.hist-labeled_list.thr10freq.neg.CPG.txt"), sep = "\t", header=FALSE)
-GNG_events = read.delim(paste0(results_dir, "/splicing_events.hist-labeled_list.thr10freq.neg.GNG.txt"), sep = "\t", header=FALSE)
-EPN_events = read.delim(paste0(results_dir, "/splicing_events.hist-labeled_list.thr10freq.neg.EPN.txt"), sep = "\t", header=FALSE)
-HGG_events = read.delim(paste0(results_dir, "/splicing_events.hist-labeled_list.thr10freq.neg.HGG.txt"), sep = "\t", header=FALSE)
-MB_events = read.delim(paste0(results_dir, "/splicing_events.hist-labeled_list.thr10freq.neg.MB.txt"), sep = "\t", header=FALSE)
-LGG_events = read.delim(paste0(results_dir, "/splicing_events.hist-labeled_list.thr10freq.neg.LGG.txt"), sep = "\t", header=FALSE)
+## select and create list for exon inclusion only events
+splice_event_df_ATRT <- splice_event_df %>% filter(histology=='ATRT', type=='inclusion') %>% select(splicing_event)
+splice_event_df_CPG <- splice_event_df %>% filter(histology=='CPG', type=='inclusion') %>% select(splicing_event)
+splice_event_df_GNG <- splice_event_df %>% filter(histology=='GNG', type=='inclusion') %>% select(splicing_event)
+splice_event_df_EPN <- splice_event_df %>% filter(histology=='EPN', type=='inclusion') %>% select(splicing_event)
+splice_event_df_HGG <- splice_event_df %>% filter(histology=='HGG', type=='inclusion') %>% select(splicing_event)
+splice_event_df_MB <- splice_event_df %>% filter(histology=='MB', type=='inclusion') %>% select(splicing_event)
+splice_event_df_LGG <- splice_event_df %>% filter(histology=='LGG', type=='inclusion') %>% select(splicing_event)
 
-listInput_ei <- list("ATRT" =ATRT_events$V1, 
-                  "CPG" =CPG_events$V1,
-                  "GNG" =GNG_events$V1,
-                  "EPN" =EPN_events$V1,
-                  "HGG" =HGG_events$V1, 
-                  "MB" =MB_events$V1,
-                  "LGG"=LGG_events$V1)
+list_for_inclusion_upsetR <- list("ATRT"=splice_event_df_ATRT$splicing_event,
+                                 "CPG"=splice_event_df_CPG$splicing_event,
+                                 "GNG"=splice_event_df_GNG$splicing_event,
+                                 "EPN"=splice_event_df_EPN$splicing_event,
+                                 "HGG"=splice_event_df_HGG$splicing_event,
+                                 "MB"=splice_event_df_MB$splicing_event,
+                                 "LGG"=splice_event_df_LGG$splicing_event)
 
-
-ei_events <- upset(fromList(listInput_ei), 
-      mainbar.y.label = "", sets.x.label = "Histology", order.by = "freq",
-      mb.ratio = c(0.5,0.50), text.scale = c(1.3, 1.3, 1.3, 1.3, 2, 1.4),point.size = 3, line.size = 1.5, nsets = 7)
+## make upsetR plots
+ei_events <- upset(fromList(list_for_inclusion_upsetR), 
+                   mainbar.y.label = "", sets.x.label = "Histology", order.by = "freq",
+                   mb.ratio = c(0.5,0.50), text.scale = c(1.3, 1.3, 1.3, 1.3, 2, 1.4),point.size = 3, line.size = 1.5, nsets = 7)
 
 # Save plot as PDF
 pdf(upsetR_ei_plot_file, width = 16, height = 8)
