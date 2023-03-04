9a38a5690fb7b97865fc272fcc7fdbf448be6d99
Ammar S. Naqvi <naqvia@email.chop.edu>
2023-03-04 11:08:56 -0500

    script to plot tmb of high vs low SBI tumors

---
+++ analyses/splicing_index/06-plot_tmb_by_sbi.R
@@ -0,0 +1,58 @@
+# 06-plot_tmb_by_sbi.R
+# written by Ammar Naqvi
+#
+# This script plots TMB based on high vs low splicing burden 
+#
+# usage: Rscript 06-plot_tmb_by_sbi.R
+################################################################################
+
+## load libraries
+suppressPackageStartupMessages({
+  library("tidyverse")
+  library("optparse")
+  library("vroom")
+  library("ggplot2")
+  library("ggpubr")
+} )
+
+#Get `magrittr` pipe
+`%>%` <- dplyr::`%>%`
+
+## set up directories
+root_dir <- rprojroot::find_root(rprojroot::has_dir(".git"))
+analysis_dir <- file.path(root_dir, "analyses","splicing_index")
+input_dir   <- file.path(analysis_dir, "input")
+data_dir   <- file.path(root_dir, "input")
+
+## get and setup input
+tmb_coding_file  <- file.path(input_dir,"tmb-coding.tsv")
+tmb_coding_df  <-  vroom(tmb_coding_file, comment = "#",delim="\t")  %>% mutate(Kids_First_Biospecimen_ID=Tumor_Sample_Barcode)
+
+sbi_coding_file  <- file.path(results_dir,"splicing_index.total.txt")
+sbi_coding_df  <-  vroom(sbi_coding_file, comment = "#",delim="\t") %>% mutate(Kids_First_Biospecimen_ID=Tumor_Sample_Barcode) %>% filter(Histology=="HGG")
+
+clin_file = "~/d3b_coding/pbta-splicing/data/histologies.tsv"
+clin_df  <-  vroom(clin_file, comment = "#",delim="\t") 
+sbi_ids_clin <- clin_df %>% inner_join(sbi_coding_df, by="Kids_First_Biospecimen_ID") %>% filter(tumor_descriptor=="Initial CNS Tumor")
+tmb_ids_clin <- clin_df %>% inner_join(tmb_coding_df, by="Kids_First_Biospecimen_ID") %>% filter(tumor_descriptor=="Initial CNS Tumor")
+
+## intersect tmb values with SBI tumors
+sbi_vs_tmb_innerjoin_df <- left_join(sbi_ids_clin,tmb_ids_clin, by="Kids_First_Participant_ID") 
+
+## compute quantiles to define high vs low Exon 4 SBI tumors
+quartiles_sbi <- quantile(sbi_vs_tmb_innerjoin_df$SI, probs=c(.25, .75), na.rm = FALSE)
+IQR_sbi <- IQR(sbi_vs_tmb_innerjoin_df$SI)
+lower_sbi <- quartiles_sbi[1]
+upper_sbi <- quartiles_sbi[2]
+
+## subset tmb values and samples by high vs low SBI tumors
+high_sbi_df <- dplyr::filter(sbi_vs_tmb_innerjoin_df, SI > upper_sbi) %>% dplyr::mutate(SBI_level="high")
+low_sbi_df  <- dplyr::filter(sbi_vs_tmb_innerjoin_df, SI < lower_sbi) %>% dplyr::mutate(SBI_level="low")
+high_vs_low_df <- rbind(high_sbi_df,low_sbi_df)
+
+boxplot_grp_expr <- ggplot(high_vs_low_df,aes((SBI_level),log(tmb))) + 
+  geom_boxplot(aes(fill=SBI_level)) + 
+  stat_compare_means(method = "t.test") + 
+  geom_jitter() + theme_Publication()
+
+
