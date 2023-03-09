16de667bd606726e4a5cddf0c0629106675d5308
rjcorb <corbettr@chop.edu>
2023-03-09 15:44:32 -0500

    update bash script

--- analyses/survival/run-survival-module.sh
+++ analyses/survival/run-survival-module.sh
@@ -6,8 +6,9 @@ set -o pipefail
 # run prepare-survival script
 Rscript -e "rmarkdown::render('01-prepare-survival.Rmd')"
 
-# run run-survival script
-Rscript -e "rmarkdown::render('02-run-survival.Rmd')"
+# run run-survival scripts
+Rscript -e "rmarkdown::render('02-run-survival-SIgroup.Rmd')"
+Rscript -e "rmarkdown::render('03-run-survival-SI.Rmd')"
 
 # run plot-survival script
-Rscript 03-plot-survival.R
+Rscript 04-plot-survival.R
