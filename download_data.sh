f19a95de44e3055c4cf13ae9a7ed1535b7a43916
Ammar S. Naqvi <naqvia@email.chop.edu>
2023-05-26 11:13:57 -0400

    added rmats comparison data file

--- download_data.sh
+++ download_data.sh
@@ -4,8 +4,8 @@ set -o pipefail
 
 # Use the OpenPBTA bucket as the default.
 URL=${SPLICE_URL:-https://s3.amazonaws.com/d3b-openaccess-us-east-1-prd-pbta/pbta-splicing}
-RELEASE=${SPLICE_RELEASE:-v4}
-PREVIOUS=${SPLICE_RELEASE:-v3}
+RELEASE=${SPLICE_RELEASE:-v5}
+PREVIOUS=${SPLICE_RELEASE:-v4}
 
 # Remove old symlinks in data
 find data -type l -delete
