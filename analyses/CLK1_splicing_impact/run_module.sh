34671cba82e304c58a4823e2686e0047401790a8
Ammar S. Naqvi <naqvia@email.chop.edu>
2023-08-01 14:51:01 -0400

    gsea code and run script

---
+++ analyses/CLK1_splicing_impact/run_module.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+set -e
+set -o pipefail
+
+## perform diff expression on ctrl vs clk1-morph
+Rscript 01-diffExpr-ctrl_vs_morph.R
+echo -e "gene\tbaseMean\tlog2FoldChange\tlfcSE\tstat\tpvalue\tpadj\tSignificant" > results/ctrl_vs_treated.de.formatted.tsv
+cat results/ctrl_vs_treated.de.tsv | awk -F "\t" '{print $8,"\t"$1"\t"$2"\t"$3"\t"$4"\t"$6"\t"$7}' | awk -F "_" '{print $2}' >> results/ctrl_vs_treated.de.formatted.tsv
+
+## perform GSEA on diff expression results
+Rscript 02-gsea-analysis.R
