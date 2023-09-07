e4d701835907b59385e0b5e1e2b8a5212f654fe1
Ammar S. Naqvi <naqvia@email.chop.edu>
2023-09-07 12:08:24 -0400

    collapse scripts into one script and cleaned up dir

--- analyses/splicing_index/02-plot_splicing_burden_index.R
+++ analyses/splicing_index/02-plot_splicing_burden_index.R
@@ -1,12 +1,11 @@
 ################################################################################
-# splicing_index_tumors.R
-# script that takes in "splicing_index.total.txt" data file
-# and computes relative proportion (splicing burden index of aberrant splicing 
-# changes in samples
+# 07-plot_splicing_burden_index.R
+# script that takes in SBI tsv files and  computes relative proportion 
+# (splicing burden index of aberrant splicing changes in samples
 #
 # written by Ammar Naqvi
 #
-# usage: Rscript splicing_index_tumors.R
+# usage: Rscript 07-plot_splicing_burden_index.R
 ################################################################################
 
 suppressPackageStartupMessages({
@@ -33,7 +32,10 @@ if(!dir.exists(plots_dir)){
   dir.create(plots_dir, recursive=TRUE)
 }
 
-file_si_plot = "SI_total.tiff"
+file_si_SE_plot = "SBI-plot.SE.tiff"
+file_si_RI_plot = "SBI-plot.RI.tiff"
+file_si_A5SS_plot = "SBI-plot.A5SS.tiff"
+file_si_A3SS_plot = "SBI-plot.A3SS.tiff"
 
 # theme for all plots
 # source functions
@@ -41,61 +43,77 @@ figures_dir <- file.path(root_dir, "figures")
 source(file.path(figures_dir, "theme_for_plots.R"))
 
 # read in SI file
-splice_index_file <- file.path(results_dir, "splicing_index.total.txt")
-splice_index_df <- readr::read_tsv(splice_index_file)
+splice_index_SE_file   <- file.path(results_dir, "splicing_index.SE.txt")
+splice_index_RI_file   <- file.path(results_dir, "splicing_index.RI.txt")
+splice_index_A5SS_file <- file.path(results_dir, "splicing_index.A5SS.txt")
+splice_index_A3SS_file <- file.path(results_dir, "splicing_index.A3SS.txt")
+
+splice_index_SE_df   <- readr::read_tsv(splice_index_SE_file)
+splice_index_RI_df   <- readr::read_tsv(splice_index_RI_file)
+splice_index_A5SS_df <- readr::read_tsv(splice_index_A5SS_file)
+splice_index_A3SS_df <- readr::read_tsv(splice_index_A3SS_file)
 
 
-splice_index <- splice_index_df %>%
-  as.data.frame(stringsAsFactors = FALSE)
-
-# Set up the data.frame for plotting
-si_cdf_plot <- splice_index %>%
-  # Group by specified column
-  dplyr::group_by(Histology) %>%
-  # Only keep groups with the specified minimum number of samples
-  dplyr::filter(dplyr::n() > 1) %>%
+plot_sbi <- function(sbi_df, plot_file) { ## if conflicting intrep. take the call with most calls in CLNSIGCONF field
   
-  # Calculate group median
-  dplyr::mutate(
-    group_median = median(SI, na.rm = TRUE),
-    group_rank = rank(SI, ties.method = "first") / dplyr::n(),
-    sample_size = paste0("n = ", dplyr::n())
-  ) %>%
-  dplyr::ungroup() %>%
-  dplyr::mutate(Histology = reorder(Histology, group_median)) 
+  # Set up the data.frame for plotting
+  si_cdf_plot <- sbi_df %>%
+    # Group by specified column
+    dplyr::group_by(Histology) %>%
+    # Only keep groups with the specified minimum number of samples
+    dplyr::filter(dplyr::n() > 1) %>%
+    # Calculate group median
+    dplyr::mutate(
+      group_mean = mean(SI, na.rm = TRUE),
+      group_rank = rank(SI, ties.method = "first") / dplyr::n(),
+      sample_size = paste0("n = ", dplyr::n())
+    ) %>%
+    dplyr::ungroup() %>%
+    dplyr::mutate(Histology = reorder(Histology, group_mean))
 
-si_plot <- si_cdf_plot %>%
-  # Now we will plot these as cumulative distribution plots
-  ggplot2::ggplot(ggplot2::aes(
-    x = group_rank,
-    y = log2(SI*100)
-  )) +
-  
-  ggplot2::geom_point(color = "black", alpha =0.7, shape = 1) +
-  
-  # Add summary line for median
-  ggplot2::geom_segment(
-    x = 0, xend = 1, color = "red",linetype=2,
-    ggplot2::aes(y = log2(group_median*100), yend = log2(group_median*100))
-  ) +
-  
-  # Separate by histology
-  ggplot2::facet_wrap(~ Histology + sample_size, nrow = 1, strip.position = "bottom", labeller = ggplot2::label_wrap_gen(multi_line = FALSE)) +
-  ggplot2::xlab("Histology") +
-  ggplot2::ylab("Splicing Burden Index") +
-  
-  # Making it pretty
-  ggplot2::theme(legend.position = "none") +
-  theme_Publication() +
-  ggplot2::theme(
-    axis.text.x = ggplot2::element_blank(),
-    axis.ticks.x = ggplot2::element_blank(),
-    strip.placement = "outside"
-  )  
+  si_plot <- si_cdf_plot %>%
+    # Now we will plot these as cumulative distribution plots
+    ggplot2::ggplot(ggplot2::aes(
+      x = group_rank,
+      y = log2(SI * 100)
+    )) +
+    ggplot2::geom_point(color = "black", alpha = 0.7, shape = 1) +
 
-dev.set(dev.next())
+    # Add summary line for median
+    ggplot2::geom_segment(
+      x = 0, xend = 1, color = "red", linetype = 2,
+      ggplot2::aes(y = log2(group_mean * 100), yend = log2(group_mean * 100))
+    ) +
 
-# Save plot
-tiff(file.path(plots_dir, file_si_plot), height = 1200, width = 3000, res = 300)
-print(si_plot)
-dev.off()
+    # Separate by histology
+    ggplot2::facet_wrap(~ Histology + sample_size, nrow = 1, strip.position = "bottom", labeller = ggplot2::label_wrap_gen(multi_line = FALSE)) +
+    ggplot2::xlab("Histology") +
+    ggplot2::ylab("Splicing Burden Index") +
+
+    # Making it pretty
+    ggplot2::theme(legend.position = "none") +
+    theme_Publication() +
+    ggplot2::theme(
+      axis.text.x = ggplot2::element_blank(),
+      axis.ticks.x = ggplot2::element_blank(),
+      strip.placement = "outside"
+    )
+
+  dev.set(dev.next())
+
+  # Save plot
+  tiff(file.path(plots_dir, plot_file), height = 1200, width = 3000, res = 300)
+  print(si_plot)
+  dev.off()
+}
+
+## plot SBI for each splicing case
+splice_index_SE_df   <- readr::read_tsv(splice_index_SE_file) %>% as.data.frame(stringsAsFactors = FALSE)
+splice_index_RI_df   <- readr::read_tsv(splice_index_RI_file) %>% as.data.frame(stringsAsFactors = FALSE)
+splice_index_A5SS_df <- readr::read_tsv(splice_index_A5SS_file) %>% as.data.frame(stringsAsFactors = FALSE)
+splice_index_A3SS_df <- readr::read_tsv(splice_index_A3SS_file) %>% as.data.frame(stringsAsFactors = FALSE)
+
+plot_sbi(splice_index_SE_df,file_si_SE_plot)
+plot_sbi(splice_index_RI_df,file_si_RI_plot)
+plot_sbi(splice_index_A5SS_df,file_si_A5SS_plot)
+plot_sbi(splice_index_A3SS_df,file_si_A3SS_plot)
