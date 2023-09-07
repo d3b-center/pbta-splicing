225c98ef015c1d79c231e78cab959b76c5da5459
Ammar S. Naqvi <naqvia@email.chop.edu>
2023-09-07 12:08:24 -0400

    collapse scripts into one script and cleaned up dir

--- analyses/splicing_index/01-generate_SBI.sh
+++ analyses/splicing_index/01-generate_SBI.sh
@@ -7,11 +7,11 @@ indep_samples="../../data/independent-specimens.rnaseqpanel.primary.tsv"
 indep_samples_plus="../../data/independent-specimens.rnaseqpanel.primary-plus.tsv"
 
 ## process PSI and generate SBI tables
-perl 03-generate_splicing-index_and_diff-events_table.SE.pl $hist_file $rmats_file $indep_samples $indep_samples_plus
-perl 04-generate_splicing-index_and_diff-events_table.RI.pl $hist_file $rmats_file $indep_samples $indep_samples_plus
-perl 05-generate_splicing-index_and_diff-events_table.A5SS.pl $hist_file $rmats_file $indep_samples $indep_samples_plus
-perl 06-generate_splicing-index_and_diff-events_table.A3SS.pl $hist_file $rmats_file $indep_samples $indep_samples_plus
+perl 01-generate_splicing-index_and_diff-events_table.pl $hist_file $rmats_file $indep_samples $indep_samples_plus SE
+perl 01-generate_splicing-index_and_diff-events_table.pl $hist_file $rmats_file $indep_samples $indep_samples_plus A3SS
+perl 01-generate_splicing-index_and_diff-events_table.pl $hist_file $rmats_file $indep_samples $indep_samples_plus A5SS
+perl 01-generate_splicing-index_and_diff-events_table.pl $hist_file $rmats_file $indep_samples $indep_samples_plus RI
 
 ## plot values (SBI) generated from above script in CDF plot
 echo "plotting splicing burden indices"
-Rscript 07-plot_splicing_burden_index.R
+Rscript 02-plot_splicing_burden_index.R
