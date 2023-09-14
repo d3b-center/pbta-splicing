a1dc19a86947cc1feee52094ad46d0111f4d135e
Ammar S. Naqvi <naqvia@email.chop.edu>
2023-09-14 17:21:13 -0400

    fixed typo

--- analyses/CLK1_splicing_impact/02-format-deseq2-res.sh
+++ analyses/CLK1_splicing_impact/02-format-deseq2-res.sh
@@ -1,4 +1,4 @@
 ## Format DESeq2 results for GSEA script
 
-echo "gene      baseMean        log2FoldChange  lfcSE   stat    pvalue  padj    Significant" > results/ctrl_vs_treated.de.formatted.ts
+echo "gene      baseMean        log2FoldChange  lfcSE   stat    pvalue  padj    Significant" > results/ctrl_vs_treated.de.formatted.tsv
 cat results/ctrl_vs_treated.de.tsv|  awk -F "\t" '{print $8"\t"$1"\t"$2"\t"$3"\t"$4"\t"$5"\t"$6"\t"$7}' | awk -F "_" '{print $2}'  >> results/ctrl_vs_treated.de.formatted.tsv
