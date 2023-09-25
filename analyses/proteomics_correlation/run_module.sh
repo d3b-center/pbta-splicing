bfaaadb5d34349573ec4640048fee08a7e7327b5
Ammar S. Naqvi <naqvia@email.chop.edu>
2023-09-25 18:20:19 -0400

    script to run analysis

---
+++ analyses/proteomics_correlation/run_module.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+## get relevant splice events
+Rscript 01-identify-and-create-splice-variant-matrix.R
+
+## create table with PSIs
+perl 02-add_psi_to_table.pl
+
+## convert table to bed for peptide sequence extraction
+cat results/mixed_events.tsv | awk '{print $2"\t"$3"\t"$4"\t"$6"\t0\t"$5}' | grep "_" > results/mixed_events.bed
+
+## get nucleotide sequence
+bedtools getfasta -fi ~/Downloads/hg38.fa -bed results/mixed_events.bed > results/mixed_events.fasta
+
+perl 03-convert_to_peptide.pl results/mixed_events.fasta
