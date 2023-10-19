6a627a15dc31a7029080087e4f1182cf46509496
JosephDybas <joseph.dybas@gmail.com>
2023-10-19 09:42:07 -0400

    RNA splicing and Phospho proteomics Psi-Phos comparison
    This commit includes the input data and R script required to produce a scatterplot comparing the RNA differential splicing Psi values and corresponding phospho proteomics abundances for a given differential splicing event.

---
+++ analyses/proteomics_correlation/RNAsplicing-PHOSproteomics-scatterplots.R
@@ -0,0 +1,108 @@
+
+
+#load libraries
+library(tidyverse)
+library(openxlsx)
+
+#Magrittr pipe
+`%>%` <- dplyr::`%>%`
+
+#directory containing input files
+input_dir <- "./input"
+#directory to store plots
+plot_dir <- "./plots"
+
+#read histology file to data frame
+histologyfile <- "Hope-GBM-histologies-base.tsv"
+HISTdata <- readr::read_tsv(file.path(input_dir, histologyfile))
+#
+
+######read whole cell proteomics data to data frame
+#####totalprotfile <- "Hope_proteome_imputed_data_liftover.tsv"
+#####WCPdata <- readr::read_tsv(file.path(input_dir, totalprotfile))
+######
+#####
+######get all WCP samples ids
+#####WCP_KFid <- colnames(WCPdata)[4:ncol(WCPdata)]
+######
+
+#read phosphoproteomics data to data frame
+phosprotfile <- "Hope_phosphosite_imputed_data_ischemia_removed_liftover.tsv"
+PHOSdata <- readr::read_tsv(file.path(input_dir, phosprotfile))
+#
+
+#get all PHOS samples ids
+PHOS_KFid <- colnames(PHOSdata)[9:ncol(PHOSdata)]
+#
+
+#read splicing events data to data frame
+splicingfile <- "splicing_events-psi.tsv"
+SPLICEdata <- readr::read_tsv(file.path(input_dir, splicingfile))
+#
+
+#get sample IDs from splicing file. these are the IDs used in the HGG splicing analysis
+SPLICE_KFid <- colnames(SPLICEdata)[9:ncol(SPLICEdata)]
+#
+
+
+#function to draw scatter plots of psi values vs phos abundance for specific splicing events
+MakePsiPhosScatterPlots <- function(select_gene, select_site, HISTdata, SPLICEdata, SPLICE_KFid, PHOSdata, PHOS_KFid){
+  
+  #make lookup table of splice KF ids and corresponding sample id
+  SpliceID_SampleID_lookup <- HISTdata %>%
+    dplyr::filter(Kids_First_Biospecimen_ID%in%SPLICE_KFid) %>%
+    dplyr::select(Kids_First_Biospecimen_ID,sample_id)
+  
+  #make lookup table of phos KF ids and corresponding sample id
+  PhosID_SampleID_lookup <- HISTdata %>%
+    dplyr::filter(Kids_First_Biospecimen_ID%in%PHOS_KFid) %>%
+    dplyr::select(Kids_First_Biospecimen_ID,sample_id)
+  
+  #create scatter plot to compare splicing psi values to phosphorylation abundance
+  #data frame to store psi and corresonding phos abundance values for each HOPE sample
+  Psi_Phos_Data <- tibble("Psi"=numeric(),"Phos"=numeric())
+  #parse each splice sample and find corresponding phos data
+  for ( i in 1:length(SPLICE_KFid) ){
+    
+    #current splicing kids first ID
+    currSpliceKFid <- SPLICE_KFid[i]
+    
+    #corresponding sample_id
+    currSampleId <- dplyr::filter(SpliceID_SampleID_lookup, Kids_First_Biospecimen_ID %in% {{currSpliceKFid}}) %>% pull(sample_id)
+    #if the sample_ID does not exist in the HOPE cohort move to next
+    if ( length(currSampleId)==0 ){ next }
+    
+    #phosphorylation kids first ID corresponding to current sample_ID
+    currPhosKFid <- dplyr::filter(PhosID_SampleID_lookup, sample_id %in% {{currSampleId}}) %>% pull(Kids_First_Biospecimen_ID)
+    #if the kids first ID does not exist in the HOPE cohort move to next
+    if ( length(currPhosKFid)==0 ){ next }
+    
+    #psi and phos data for current sample
+    curr_psi <- dplyr::filter(SPLICEdata, gene %in% {{select_gene}}) %>% pull({{currSpliceKFid}})
+    curr_phos <- dplyr::filter(PHOSdata, Site %in% {{select_site}}) %>% pull({{currPhosKFid}})
+    
+    #add psi and phos data to date frame
+    Psi_Phos_Data <- Psi_Phos_Data %>% add_row(Psi={{curr_psi}},Phos={{curr_phos}})
+    
+  }
+  #
+  #scatter plot
+  file_name<-paste("PsiPhosAbd", select_gene, "scatter.pdf", sep="_")
+  pdf(file.path(".", plot_dir, file_name), height=6, width=6)
+  Psi_Phos_plot<-ggplot(Psi_Phos_Data, aes(x=Psi, y=Phos)) +
+    geom_point(size=2,color="darkorchid") +
+    geom_smooth(method=lm, color="grey40") +
+    theme_classic() + theme(axis.line = element_line(colour = 'black', linewidth = 1)) +
+    scale_x_continuous(limits = c(0, 1)) + scale_y_continuous(limits = c(18.5, 20.25))
+  print(Psi_Phos_plot)
+  dev.off()
+  
+  return()
+  
+} #end MakePsiPhosScatterPlots function
+  
+#draw scatter plot of psi values and phospho abundances for selected gene and phospho site
+select_gene<-"CLK1"
+select_site<-"NP_001155879.1_182_200_1_1_S182"
+#call function to draw scatter plots of psi values vs phos abundance for specific splicing events
+MakePsiPhosScatterPlots(select_gene, select_site, HISTdata, SPLICEdata, SPLICE_KFid, PHOSdata, PHOS_KFid)
