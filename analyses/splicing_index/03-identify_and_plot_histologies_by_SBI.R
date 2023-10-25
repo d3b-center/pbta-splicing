2f99ae4cbb0ab59e899bbf78e56bd5f46700bafe
jharenza <jharenza@gmail.com>
2023-10-25 22:32:39 +0000

    fix list and input for color palette

--- analyses/splicing_index/03-identify_and_plot_histologies_by_SBI.R
+++ analyses/splicing_index/03-identify_and_plot_histologies_by_SBI.R
@@ -10,7 +10,6 @@
 ## load libraries
 suppressPackageStartupMessages({
   library("tidyverse")
-  library("vroom")
   library("ggplot2")
 })
 
@@ -32,22 +31,21 @@ source(file.path(figures_dir, "theme_for_plots.R"))
 ## define output files
 plot_path  <- file.path(plots_dir,"hist_by_sbi-level_lolliplot.pdf")
 
-## get and setup input
+## get and setup input files
 sbi_coding_file  <- file.path(results_dir,"splicing_index.SE.txt")
-sbi_coding_df  <-  vroom(sbi_coding_file, comment = "#",delim="\t") %>% 
-  dplyr::rename(Kids_First_Biospecimen_ID=Sample) 
+palette_file <- file.path(results_dir,"histologies-plot-group.tsv") 
 
-clin_file = file.path(results_dir,"histologies-plot-group.tsv") 
-histologies_df <- vroom(clin_file) %>%
-  dplyr::select(Kids_First_Biospecimen_ID, plot_group) %>% 
-  dplyr::rename(Histology = plot_group)
+# read in files, join palette with sbi file
+sbi_coding_df  <-  read_tsv(sbi_coding_file, comment = "#") %>% 
+  dplyr::rename(Kids_First_Biospecimen_ID = Sample) 
 
-sbi_coding_df %>% inner_join(sbi_coding_df, by="Kids_First_Biospecimen_ID")
+palette_df <- read_tsv(palette_file, guess_max = 100000) %>%
+  dplyr::rename(Histology = plot_group) %>%
+  select(Histology, plot_group_hex) %>%
+  unique()
 
-clin_df_w_SBI  <-  vroom(clin_file, comment = "#",delim="\t",show_col_types = FALSE) %>% 
-  inner_join(sbi_coding_df, by="Kids_First_Biospecimen_ID") %>%
-  dplyr::select(Kids_First_Biospecimen_ID, SI, plot_group) %>% 
-  dplyr::rename(Histology = plot_group)
+sbi_coding_df <- sbi_coding_df %>%
+  left_join(palette_df)
 
 ## compute quantiles to define high vs low SBI tumors
 quartiles_sbi <- quantile(sbi_coding_df$SI, probs=c(.25, .75), na.rm = FALSE)
@@ -55,8 +53,7 @@ IQR_sbi <- IQR(sbi_coding_df$SI)
 lower_sbi <- quartiles_sbi[1]
 upper_sbi <- quartiles_sbi[2]
 
-
-clin_df_w_SBI <- clin_df_w_SBI %>% 
+clin_df_w_SBI <- sbi_coding_df %>% 
   mutate(SBI_level = case_when(SI > upper_sbi ~ "High SBI", 
                                SI < lower_sbi ~ "Low SBI")) %>% 
   filter(!is.na(SBI_level))
@@ -66,29 +63,20 @@ clin_df_w_lowSBI <- clin_df_w_SBI %>% filter(SBI_level=="Low SBI")
 
 histology_counts_by_sbi <- clin_df_w_SBI %>% dplyr::count(SBI_level, Histology) %>%
   dplyr::mutate(SBI_level = fct_relevel(SBI_level,
-                                        c("Low SBI", "High SBI")))
+                                        c("Low SBI", "High SBI"))) %>%
+  left_join(palette_df)
 
+# make colors
+plot_colors <- palette_df$plot_group_hex
+names(plot_colors) <- palette_df$Histology
+plot_colors <- list(plot_colors)
 
-## create color palette 
-palette_file <- file.path(results_dir, "histologies-plot-group.tsv")
-
-## make plot colors list
-palette_df <- read_tsv(palette_file) %>%
-  dplyr::rename(Histology = plot_group) %>%
-  select(Histology, plot_group_hex) %>%
-  unique()
-
-plot_colors <- list()
-plot_colors[['Histology']] <- palette_df$plot_group_hex
-
-plot_labels <- list()
-plot_labels[['Histology']]  <- palette_df$Histology
-#names(plot_labels) <- palette_df$Histology
-
-lolliplot_plot <- ggplot(histology_counts_by_sbi, aes(x=Histology, y=n, color=Histology)) +
+# plot 
+lolliplot_plot <- ggplot(histology_counts_by_sbi, 
+                         aes(x=Histology, y=n, color=Histology)) +
   geom_segment(aes(x=Histology, xend=Histology, y=0, yend=n)) +
-  scale_color_manual(name = "Histology",values = plot_colors[['Histology']]) + 
-  geom_point(size=4) +
+  scale_color_manual(name = "Histology", values = plot_colors) + 
+  geom_point(size = 4) +
   xlab("") + ylab("# of Samples") +
   facet_wrap(SBI_level ~ .) +
   theme_Publication() + 
@@ -99,8 +87,9 @@ lolliplot_plot <- ggplot(histology_counts_by_sbi, aes(x=Histology, y=n, color=Hi
         legend.text = element_text(size=12), 
         axis.title=element_text(size=14)) 
 
+lolliplot_plot
 
 # save plot pdf version
-pdf(plot_path, height = 4, width = 14)
+pdf(plot_path, height = 4, width = 10)
 print(lolliplot_plot)
 dev.off()
