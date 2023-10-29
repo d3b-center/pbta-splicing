677930b8dd959058bcb4f1b9b70fd8069c08c882
jharenza <jharenza@gmail.com>
2023-10-29 19:21:08 +0000

    try to use the new phospho file, but many missing data points

--- analyses/CLK1_splicing_correlations/03-plot_SR-phosp_vs_CLK1-RNA.R
+++ analyses/CLK1_splicing_correlations/03-plot_SR-phosp_vs_CLK1-RNA.R
@@ -1,6 +1,6 @@
 ################################################################################
 # 03-plot_SR-phosp_vs_CLK1-RNA.R
-# written by Shehbeel Arif, Ammar Naqvi
+# written by Shehbeel Arif, Ammar Naqvi, Jo Lynne Rokita
 #
 # This script uses CPTAC data to generate and plot heatmap of RNA vs phosp of 
 # CLK1 and SRSF phospho-levels
@@ -21,7 +21,8 @@ suppressPackageStartupMessages({
 
 ## set directories
 root_dir <- rprojroot::find_root(rprojroot::has_dir(".git"))
-analysis_dir <- file.path(root_dir, "analyses", "CLK1-splicing_correlations")
+analysis_dir <- file.path(root_dir, "analyses", "CLK1_splicing_correlations")
+data_dir   <- file.path(root_dir, "data")
 
 input_dir   <- file.path(analysis_dir, "input")
 plots_dir   <- file.path(analysis_dir, "plots")
@@ -31,18 +32,49 @@ if(!dir.exists(plots_dir)){
   dir.create(plots_dir, recursive=TRUE)
 }
 
-## output files for final plots
-heatmap_output_file <- file.path(plots_dir,"heatmap_SR-phosp_CLK1-RNA_rna_prot.pdf") 
+## file paths
+clin_file <- file.path(data_dir, "histologies.tsv")
+heatmap_output_file <- file.path(plots_dir,"SR_phos_CLK1_exp_heatmap.pdf") 
+tpm_file <- file.path(data_dir, "gene-expression-rsem-tpm-collapsed.rds")
+
+# load clinical file
+clin_rna <- read_tsv(clin_file, guess_max = 100000) %>%
+  filter(cohort == "PBTA",
+         experimental_strategy == "RNA-Seq")
+
+# load TPM file for only cohort of interest
+tpm_data <- readRDS(tpm_file) %>%
+  select(clin_rna$Kids_First_Biospecimen_ID)
 
 ## get CPTAC output table 
-cptac_output_file <- file.path(input_dir,"CPTACT_SR_CLK1.xls") 
+cptac_output_file <- file.path(input_dir,"CPTAC_SR_CLK1.xls") 
+cptac_output_file2 <- file.path(input_dir,"Gygi_TCMP_HMS_Phosphoproteome.phosphosite.tmt11_RearrangedCols.csv") 
 
 # Load dataset
 cptac_data <- read_excel(cptac_output_file, na = "NA")  %>%
   # Only look at RNA and Protein data
   filter(type %in% c("rna", "phospho")) %>%
+  rename_with(~ gsub("X7316.", "7316-", .), everything()) %>%
   # remove any samples which have NA
   select_if(~ !any(is.na(.)))
+
+cptac_data2 <- read_csv(cptac_output_file2)  %>%
+  select(-c("Prot", "Peptide", "Organism")) %>%
+  # remove extra info from cols
+  rename_with(~ gsub(" Log Ratio|  Log Ratio", "", .), everything()) %>%
+  filter(grepl("SRSF", Gene) & rowSums(!is.na(.[-(1:5)])) > 0) 
+       
+setdiff(names(cptac_data), names(cptac_data2))  
+setdiff(names(cptac_data2), names(cptac_data)) 
+
+cptac_data %>%
+  filter(gene == "SRSF11") %>%
+  select(gene, all_of(names(cptac_data[,3:8])))
+
+cptac_data2 %>%
+  filter(Gene == "SRSF11") %>%
+  select(Gene, all_of(names(cptac_data[,3:8])))
+
 
 # some genes have duplicate phos sites, so make them unique ids for later rownaming
 cptac_data$suffix <- ave(cptac_data$gene, cptac_data$gene, FUN = function(x) 1 + seq_along(x) - 1)
