a43671cb947c978708181a209e7c6b5459bdfa87
Ammar S. Naqvi <naqvia@email.chop.edu>
2023-10-30 11:11:31 -0400

    uploaded and raw and added code to format raw results

--- analyses/KNS42_cell-line/03-plot-qPCR-res.R
+++ analyses/KNS42_cell-line/03-plot-qPCR-res.R
@@ -47,3 +47,45 @@ ggplot(qpcr_fc_df, aes(y=`Fold change`, x=Concentration, fill=`Splice junction`)
   theme_Publication()
 
 dev.off()
+
+
+## output for plot
+file_plot = file.path(plots_dir,"qPCR-morp.pdf")
+
+qpcr_res_file = file.path(input_dir,"qpcr-raw-triplicates.tsv")
+qpcr_fc_df <- read_tsv(qpcr_res_file) 
+
+# format and compute ddct values from raw values
+qpcr_ddct_transform <- as.data.frame(qpcr_fc_df) %>%
+  ## group by junction and calculate means
+  group_by(SJ) %>%
+  summarise_all(mean) %>%
+  
+  ## calculuate dct and ddct values
+  mutate(across(Control:`10uM`,  ~ last(.) - . )) %>% 
+  mutate(across(`1uM`:`10uM`, ~ Control - .  )) %>% 
+  mutate(across(Control, ~ 1  )) %>% 
+  
+  ## reformat and rename to plot
+  pivot_longer(cols=c('Control','1uM','5uM','10uM'), 
+               names_to = "Concentration",
+               values_to = "ddct") %>% 
+  dplyr::filter(SJ != 'HPRT') %>% 
+  mutate("ddct" = ddct) %>%
+  mutate(`Fold Change` = (2)^-ddct) %>% 
+  mutate(`Fold Change` = case_when(Concentration=='Control' ~ 1,
+         .default = `Fold Change`))
+
+qpcr_ddct_transform$Concentration <- factor(qpcr_ddct_transform$Concentration, levels = c('Control','1uM','5uM','10uM'))
+
+# Save plot as PDF
+pdf(file_plot, width =8, height = 5)
+
+ggplot(qpcr_ddct_transform, aes(y=`Fold Change`, x=Concentration, fill=SJ)) + 
+  geom_bar(position="dodge", stat="identity",color = "black") +
+  ylab("Fold Change") + 
+  theme_Publication() + 
+  scale_fill_manual(name = "Splice Junction",values=c("#FFC20A","#0C7BDC","#0C7BDC")) 
+
+dev.off()
+
