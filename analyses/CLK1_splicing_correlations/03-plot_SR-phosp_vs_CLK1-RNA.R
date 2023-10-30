6f3fa9045ea9b342ae256ab1df780014ae6aa807
jharenza <jharenza@gmail.com>
2023-10-30 21:09:55 +0000

    update code for new matrix, but heatmap not rendering - to be cont

--- analyses/CLK1_splicing_correlations/03-plot_SR-phosp_vs_CLK1-RNA.R
+++ analyses/CLK1_splicing_correlations/03-plot_SR-phosp_vs_CLK1-RNA.R
@@ -33,76 +33,54 @@ if(!dir.exists(plots_dir)){
 }
 
 ## file paths
-clin_file <- file.path(data_dir, "histologies.tsv")
 heatmap_output_file <- file.path(plots_dir,"SR_phos_CLK1_exp_heatmap.pdf") 
-tpm_file <- file.path(data_dir, "gene-expression-rsem-tpm-collapsed.rds")
-
-# load clinical file
-clin_rna <- read_tsv(clin_file, guess_max = 100000) %>%
-  filter(cohort == "PBTA",
-         experimental_strategy == "RNA-Seq")
-
-# load TPM file for only cohort of interest
-tpm_data <- readRDS(tpm_file) %>%
-  select(clin_rna$Kids_First_Biospecimen_ID)
 
 ## get CPTAC output table 
 cptac_output_file <- file.path(input_dir,"CPTAC_SR_CLK1.xls") 
-cptac_output_file2 <- file.path(input_dir,"Gygi_TCMP_HMS_Phosphoproteome.phosphosite.tmt11_RearrangedCols.csv") 
+cptac_output_file2 <- file.path(input_dir,"CPTAC3-pbt.xls") 
 
 # Load dataset
-cptac_data <- read_excel(cptac_output_file, na = "NA")  %>%
-  # Only look at RNA and Protein data
-  filter(type %in% c("rna", "phospho")) %>%
+cptac_data <- readxl::read_excel(cptac_output_file2) %>%
+  # select only rows with CLK1 or SRFs, remove muts
+  filter(grepl("CLK1|SRSF", idx),
+         !`Data type` %in% c("mut", "cnv", "proteo")) %>%
+  # remove extra info from cols
   rename_with(~ gsub("X7316.", "7316-", .), everything()) %>%
-  # remove any samples which have NA
+  dplyr::rename(Assay = `Data type`) %>%
+  # create new display name for phospho proteins
+  mutate(phos_site = case_when(Assay == "phospho" ~ str_split(idx, "phospho", simplify = TRUE)[, 2],
+                               TRUE ~ NA_character_),
+         display_name = case_when(Assay == "phospho" ~ paste(`Gene symbol`, phos_site, sep = " "),
+                                  Assay == "proteo" ~ paste(`Gene symbol`, Assay, sep = " "),
+                                  TRUE ~ `Gene symbol`)
+  ) %>%
+  select(display_name, Assay, starts_with("7316")) %>%
   select_if(~ !any(is.na(.)))
 
-cptac_data2 <- read_csv(cptac_output_file2)  %>%
-  select(-c("Prot", "Peptide", "Organism")) %>%
-  # remove extra info from cols
-  rename_with(~ gsub(" Log Ratio|  Log Ratio", "", .), everything()) %>%
-  filter(grepl("SRSF", Gene) & rowSums(!is.na(.[-(1:5)])) > 0) 
-       
-setdiff(names(cptac_data), names(cptac_data2))  
-setdiff(names(cptac_data2), names(cptac_data)) 
-
-cptac_data %>%
-  filter(gene == "SRSF11") %>%
-  select(gene, all_of(names(cptac_data[,3:8])))
-
-cptac_data2 %>%
-  filter(Gene == "SRSF11") %>%
-  select(Gene, all_of(names(cptac_data[,3:8])))
-
-
-# some genes have duplicate phos sites, so make them unique ids for later rownaming
-cptac_data$suffix <- ave(cptac_data$gene, cptac_data$gene, FUN = function(x) 1 + seq_along(x) - 1)
-
-cptac_data_suffixed <- cptac_data %>%
-  mutate(gene = ifelse(duplicated(gene) | rev(duplicated(rev(gene))), glue::glue("{gene}-{suffix}"), gene))
 # preserve gene names for rownames
-rownames <- cptac_data_suffixed$gene
+rownames <- cptac_data$display_name
 
 # convert to matrix and then add rownames
-mat <- cptac_data_suffixed %>%
-  select(3:(ncol(cptac_data_suffixed)-1))
+mat <- cptac_data %>%
+  select(3:(ncol(cptac_data))) %>%
+  as.matrix()
+
 
 # set rownames, convert to matrix
 rownames(mat) <- rownames
-mat <- as.matrix(mat)
+# mat <- as.matrix(mat)
 
 # select row annotations
-row_annot <- cptac_data_suffixed %>%
-  select("type") %>%
-  dplyr::rename(`Assay` = type) %>%
+row_annot <- cptac_data %>%
+  select(Assay) %>%
   as.data.frame()
 
 # add rownames
 rownames(row_annot) <- rownames(mat)
-  
+
+
 # create anno colors
-anno_col <- list(`Assay` = c(rna = "#CD5C5C", phospho = "#0C7BDC"))
+anno_col <- list(Assay = c(rna = "#CD5C5C", phospho = "#0C7BDC"))
 
 # Heatmap annotation
 row_anno = rowAnnotation(df = row_annot,
@@ -110,17 +88,18 @@ row_anno = rowAnnotation(df = row_annot,
 
 
 # Make heatmap without legends
-heat_plot <- Heatmap(mat,
+heat_plot <- Heatmap(mat[,1:5],
                      name = "Z-score",
                      col = colorRamp2(c(-2, 0, 2), c("orange", "white", "purple")),
                      cluster_rows = FALSE,
-                     row_split = row_annot$type, 
+                     row_split = row_annot$Assay, 
                      column_gap = 0.5,
                      show_row_names = TRUE,
                      show_column_names = FALSE,
                      show_heatmap_legend=TRUE,
                      cluster_columns = TRUE, 
                      right_annotation = row_anno,
+                     #na_col = "lightgrey",
                      #rect_gp = gpar(col = "white"),
                      row_title = NULL, 
                      column_title = NULL, 
@@ -128,7 +107,8 @@ heat_plot <- Heatmap(mat,
 
 heat_plot
 
-pdf(heatmap_output_file, width = 7, height = 5)
+pdf(heatmap_output_file, width = 15, height = 7)
 print(heat_plot)
 dev.off()
 
+mat[1:5,1:5]
