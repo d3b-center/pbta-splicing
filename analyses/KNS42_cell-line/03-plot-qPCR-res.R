4485766ca084059ed9445eaefadb86b86154fdfe
Ammar S. Naqvi <naqvia@email.chop.edu>
2023-11-01 10:18:31 -0400

    added light blue color to plot

--- analyses/KNS42_cell-line/03-plot-qPCR-res.R
+++ analyses/KNS42_cell-line/03-plot-qPCR-res.R
@@ -28,30 +28,6 @@ source(file.path(figures_dir,"theme_for_plots.R"))
 ## output for plot
 file_plot = file.path(plots_dir,"qPCR-morp.pdf")
 
-qpcr_res_file = file.path(input_dir,"qpcr-res.tsv")
-qpcr_fc_df <- vroom(qpcr_res_file, delim="\t") %>%  pivot_longer(cols=c('Control','1uM','5uM','10uM'), 
-                                                                 names_to = "Concentration",
-                                                                 values_to = "Fold change") 
-
-# specify order
-qpcr_fc_df$Concentration <- factor(qpcr_fc_df$Concentration, levels = c('Control','1uM','5uM','10uM'))
-
-
-# Save plot as PDF
-pdf(file_plot, width =8, height = 5)
-
-# generat grouped barpot
-ggplot(qpcr_fc_df, aes(y=`Fold change`, x=Concentration, fill=`Splice junction`)) + 
-  geom_bar(position="dodge", stat="identity") +
-  scale_fill_manual(values=c("#FFC20A","#0C7BDC")) + 
-  theme_Publication()
-
-dev.off()
-
-
-## output for plot
-file_plot = file.path(plots_dir,"qPCR-morp.pdf")
-
 qpcr_res_file = file.path(input_dir,"qpcr-raw-triplicates.tsv")
 qpcr_fc_df <- read_tsv(qpcr_res_file) 
 
@@ -85,7 +61,6 @@ ggplot(qpcr_ddct_transform, aes(y=`Fold Change`, x=Concentration, fill=SJ)) +
   geom_bar(position="dodge", stat="identity",color = "black") +
   ylab("Fold Change") + 
   theme_Publication() + 
-  scale_fill_manual(name = "Splice Junction",values=c("#FFC20A","#0C7BDC","#0C7BDC")) 
-
+  scale_fill_manual(name = "Splice Junction",values=c("#FFC20A","#0C7BDC","lightblue")) 
 dev.off()
 
