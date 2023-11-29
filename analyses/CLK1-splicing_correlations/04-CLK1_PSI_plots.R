c31a310faf9959e4e6a667157a1d70c78c94d2ba
Ammar S. Naqvi <naqvia@email.chop.edu>
2023-11-29 15:43:04 -0500

    modified code to get approp. sample_ids and print on plot

--- analyses/CLK1-splicing_correlations/04-CLK1_PSI_plots.R
+++ analyses/CLK1-splicing_correlations/04-CLK1_PSI_plots.R
@@ -41,38 +41,46 @@ CLK1_plot_path <- file.path(plots_dir, "CLK1_hgg_stacked.pdf")
 
 ## get SRSF11 psi values in tumors and ctrls
 indep_file <- file.path(data_dir, "independent-specimens.rnaseqpanel.primary-plus.tsv")
-indep_df <- read_tsv(indep_file)
+indep_df <- read_tsv(indep_file) %>% dplyr::filter(cohort=='PBTA')
 
 rmats_file <- file.path(data_dir,"rMATS_merged.comparison.tsv.gz")
+hist_file <- file.path(data_dir,"histologies.tsv")
+
+## load histologies info
+histologies_df <- vroom(hist_file, comment = "#", delim="\t") %>% 
+  dplyr::filter(Kids_First_Biospecimen_ID %in% indep_df$Kids_First_Biospecimen_ID)
 
 ## load rmats input for CLK1
 clk1_rmats <- vroom(rmats_file, comment = "#", delim="\t") %>%
   # filter for CLK1 and exon 4
-  filter(geneSymbol=="CLK1",
+  dplyr::filter(geneSymbol=="CLK1",
          exonStart_0base=="200860124", 
          exonEnd=="200860215") %>% 
-  filter(FDR < 0.05, PValue < 0.05) %>% 
+  dplyr::filter(FDR < 0.05, PValue < 0.05) %>% 
   mutate("Inclusion"=IncLevel2) %>% 
   mutate("Skipping"=1-IncLevel2) %>% 
-  select(sample_id, Inclusion, Skipping) %>%
+  dplyr::select(sample_id, Inclusion, Skipping) %>%
+  dplyr::rename(Kids_First_Biospecimen_ID=sample_id) %>% 
   ## reformat and rename to plot
-  pivot_longer(!sample_id, 
+  pivot_longer(!Kids_First_Biospecimen_ID, 
                names_to = "Type",
                values_to = "PSI") %>% 
-  dplyr::rename(Kids_First_Biospecimen_ID=sample_id) %>% 
-  filter(Kids_First_Biospecimen_ID %in% indep_df$Kids_First_Biospecimen_ID)
-
+  dplyr::filter(Kids_First_Biospecimen_ID %in% histologies_df$Kids_First_Biospecimen_ID) %>% 
+  right_join(histologies_df, by='Kids_First_Biospecimen_ID') %>% 
+  dplyr::select(sample_id, Type, PSI) 
+  
 # Make a column for exposure of interest to order samples by
 samples_in_order <- clk1_rmats %>%
-  filter(Type == "Inclusion") %>%
-  select(PSI, Kids_First_Biospecimen_ID) %>%
-  arrange(-PSI) %>%
-  pull(Kids_First_Biospecimen_ID)
+  dplyr::filter(Type == "Inclusion") %>%
+  dplyr::select(PSI, sample_id) %>%
+  dplyr::arrange(-PSI) %>%
+  dplyr::pull(sample_id)
 
 # create df for plotting and reorder
 plot_df <- clk1_rmats %>%
-mutate(Kids_First_Biospecimen_ID = factor(sample_id,
-                          levels = samples_in_order))
+mutate(sample_id = factor(sample_id,
+                          levels = samples_in_order)) %>% 
+  na.omit()
 
 stacked_barplot <- ggplot(plot_df, aes(x = sample_id, y = PSI, fill= Type )) +
   geom_bar(position="stack", stat="identity") + 
