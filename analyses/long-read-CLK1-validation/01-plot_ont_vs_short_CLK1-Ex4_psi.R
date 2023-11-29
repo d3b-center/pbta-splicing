22c955a244f8491e54b82176a56f10baea069b35
Ammar S. Naqvi <naqvia@email.chop.edu>
2023-11-29 12:21:51 -0500

    added raw files and processed and formatted in module

--- analyses/long-read-CLK1-validation/01-plot_ont_vs_short_CLK1-Ex4_psi.R
+++ analyses/long-read-CLK1-validation/01-plot_ont_vs_short_CLK1-Ex4_psi.R
@@ -32,21 +32,75 @@ if(!dir.exists(plots_dir)){
 }
 
 # theme for all plots
-# source functions
 figures_dir <- file.path(root_dir, "figures")
 source(file.path(figures_dir, "theme_for_plots.R"))
 
-## ont vs short tsv
-ont_vs_rmats_comparison_file <- "ont_vs_rmats.ggplot.tsv"
-ont_rmats_df  <-  vroom(file.path(input_dir,ont_vs_rmats_comparison_file), comment = "#",delim="\t", col_names = TRUE, trim_ws = TRUE, show_col_types = FALSE)
+## output plot path
+file_barplot = file.path(plots_dir,"isoform-stacked-barplot.pdf")
 
-# stacked barplot
-tiff(file.path(plots_dir, "isoform_stackedbarplot.tiff"), height = 1800, width = 1600, units = "px", res = 300)
-ggplot(ont_rmats_df, aes(fill=isoform, y=PSI, x=type)) + 
+## retrive stringtie2 results for each cell line
+cl_7316_1763_file = file.path(input_dir,"7316_1763.CLK1.processed.txt")
+cl_7316_1769_file = file.path(input_dir,"7316_1769.CLK1.processed.txt")
+cl_KNS42_file     = file.path(input_dir,"KNS42.CLK1.processed.txt")
+
+## format and process cell line information 
+cl_7316_1763_df <- vroom(cl_7316_1763_file,comment = "#", 
+                         delim = "\t", 
+                         col_names = c("transcript","tpm"), show_col_types = FALSE) %>% 
+                   dplyr::mutate(cell_line="7316_1763") %>%
+                   dplyr::filter(transcript=='STRG.1.2'| transcript=='STRG.1.1' ) %>% 
+                   dplyr::mutate(Isoform = case_when(
+                                transcript == "STRG.1.2" ~ "Inclusion",
+                                TRUE ~ "Skipping")) %>% 
+                  dplyr::mutate(type="long") %>% 
+                  dplyr::mutate(PSI = (tpm / sum(tpm)) * 100 ) %>% 
+                  # manual add rMATs results for short reads
+                  add_row( cell_line = "7316_1763", type = "short", Isoform="Skipping", PSI = 59.8) %>%
+                  add_row( cell_line = "7316_1763", type = "short", Isoform="Inclusion", PSI = 40.2) 
+  
+cl_7316_1769_df <- vroom(cl_7316_1769_file,comment = "#", 
+                         delim = "\t", 
+                         col_names = c("transcript","tpm"), show_col_types = FALSE) %>% 
+                    mutate(cell_line="7316_1769") %>% 
+                    dplyr::filter(transcript=='STRG.2.2'| transcript=='STRG.2.1' ) %>% 
+                    dplyr::mutate(Isoform = case_when(
+                                  transcript == "STRG.2.1" ~ "Inclusion",
+                                  TRUE ~ "Skipping")) %>% 
+                    dplyr::mutate(type="long") %>% 
+                    dplyr::mutate(PSI = (tpm / sum(tpm)) * 100 ) %>% 
+                    # manual add rMATs results for short reads
+                    add_row( cell_line = "7316_1769", type = "short", Isoform="Skipping", PSI = 14.9) %>%
+                    add_row( cell_line = "7316_1769", type = "short", Isoform="Inclusion", PSI = 85.1) 
+
+cl_KNS42_df <- vroom(cl_KNS42_file,comment = "#", 
+                         delim = "\t", 
+                         col_names = c("transcript","tpm"), show_col_types = FALSE) %>% 
+                         mutate(cell_line="KNS42")  %>% 
+                         dplyr::mutate(Isoform = case_when(
+                                       transcript == "STRG.1.2" ~ "Inclusion",
+                                       TRUE ~ "Skipping")) %>% 
+                        dplyr::mutate(type="long") %>% 
+                        dplyr::mutate(PSI = (tpm / sum(tpm)) * 100 ) %>% 
+                        # manual add rMATs results for short reads
+                        add_row( cell_line = "KNS42", type = "short", Isoform="Skipping", PSI = 12.9) %>%
+                        add_row( cell_line = "KNS42", type = "short", Isoform="Inclusion", PSI = 87.1) 
+
+
+cell_lines_df <- rbind(cl_KNS42_df,cl_7316_1769_df,cl_7316_1763_df)
+
+## make plot
+stacked_barplot <- ggplot(cell_lines_df, aes(fill=Isoform, y=PSI, x=type)) + 
   geom_bar(position="stack", stat="identity") +
   scale_fill_manual(values=c("#FFC20A","#0C7BDC")) +
-  facet_wrap(~cell_lines) +
-  xlab("RNA-Seq sequencing strategy") + theme_Publication()
+  facet_wrap(~cell_line) +
+  xlab("RNA-Seq sequencing strategy") + 
+  theme_Publication()
+
+pdf(file_barplot, 
+    width = 7, height = 7)
+stacked_barplot
 dev.off()
+
+
 
 
