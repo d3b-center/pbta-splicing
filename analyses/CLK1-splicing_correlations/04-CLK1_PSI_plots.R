5be97979f2ce20b526ae13155d5944f7f3e2347c
Ammar S. Naqvi <naqvia@email.chop.edu>
2023-12-04 16:29:24 -0500

    cleaned up code and changed input
    changed independent specimen file to "independent-specimens.rnaseqpanel.primary.tsv" and addressed comments

--- analyses/CLK1-splicing_correlations/04-CLK1_PSI_plots.R
+++ analyses/CLK1-splicing_correlations/04-CLK1_PSI_plots.R
@@ -32,22 +32,22 @@ if(!dir.exists(plots_dir)){
   dir.create(plots_dir, recursive=TRUE)
 }
 
-##theme for all plots
+## theme for all plots
 figures_dir <- file.path(root_dir, "figures")
 source(file.path(figures_dir, "theme_for_plots.R"))
 
 ## output files for final plots
-CLK1_plot_path <- file.path(plots_dir, "CLK1_hgg_stacked.pdf")
+CLK1_plot_path <- file.path(plots_dir, "CLK1_exon4_inclustion_fraction_hgg_stacked.pdf")
 
 ## get SRSF11 psi values in tumors and ctrls
-indep_file <- file.path(data_dir, "independent-specimens.rnaseqpanel.primary-plus.tsv")
+indep_file <- file.path(data_dir, "independent-specimens.rnaseqpanel.primary.tsv")
 indep_df <- read_tsv(indep_file) %>% dplyr::filter(cohort=='PBTA')
 
 rmats_file <- file.path(data_dir,"rMATS_merged.comparison.tsv.gz")
 hist_file <- file.path(data_dir,"histologies.tsv")
 
 ## load histologies info
-histologies_df <- vroom(hist_file, comment = "#", delim="\t") %>% 
+histologies_df <- vroom(hist_file) %>% 
   dplyr::filter(Kids_First_Biospecimen_ID %in% indep_df$Kids_First_Biospecimen_ID)
 
 ## load rmats input for CLK1
@@ -55,10 +55,11 @@ clk1_rmats <- vroom(rmats_file, comment = "#", delim="\t") %>%
   # filter for CLK1 and exon 4
   dplyr::filter(geneSymbol=="CLK1",
          exonStart_0base=="200860124", 
-         exonEnd=="200860215") %>% 
-  dplyr::filter(FDR < 0.05, PValue < 0.05) %>% 
-  mutate("Inclusion"=IncLevel2) %>% 
-  mutate("Skipping"=1-IncLevel2) %>% 
+         exonEnd=="200860215",
+         FDR < 0.05, 
+         PValue < 0.05) %>% 
+  mutate("Inclusion"=IncLevel2,
+         "Skipping"=1-IncLevel2) %>%  
   dplyr::select(sample_id, Inclusion, Skipping) %>%
   dplyr::rename(Kids_First_Biospecimen_ID=sample_id) %>% 
   ## reformat and rename to plot
@@ -80,11 +81,10 @@ samples_in_order <- clk1_rmats %>%
 plot_df <- clk1_rmats %>%
 mutate(sample_id = factor(sample_id,
                           levels = samples_in_order)) %>% 
-  na.omit()
+                          na.omit()
 
 stacked_barplot <- ggplot(plot_df, aes(x = sample_id, y = PSI, fill= Type )) +
   geom_bar(position="stack", stat="identity") + 
-  #geom_col(position = "dodge", width=0.9) + 
   scale_fill_manual(values=c("#FFC20A","#0C7BDC"), name="Exon 4") + 
   theme_Publication() + ylab(expression(bold(bolditalic("CLK1")~" Isoform Fraction"))) + xlab("Sample") + 
   theme(
