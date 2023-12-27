ecc66dc0b2ea0881b3a3321f454642a05ae854f9
jharenza <jharenza@gmail.com>
2023-12-27 22:21:49 +0000

    add code to split by mutation status

--- analyses/splicing_index/05-plot-tmb-sbi.R
+++ analyses/splicing_index/05-plot-tmb-sbi.R
@@ -8,7 +8,6 @@
 ## load libraries
 suppressPackageStartupMessages({
   library("tidyverse")
-  library("vroom")
   library("ggplot2")
   library("ggpubr")
 } )
@@ -34,55 +33,75 @@ boxplot_tmb_vs_sbi_file  <- file.path(plots_dir,"boxplot_tmb-sbi.pdf")
 ## input file
 indep_rna_file <- file.path(data_dir, "independent-specimens.rnaseqpanel.primary.tsv")
 indep_wgs_file <- file.path(data_dir, "independent-specimens.wgswxspanel.primary.prefer.wgs.tsv")
-tmb_coding_file  <- file.path(input_dir,"tmb-coding.tsv")
+# OPC v13
+tmb_coding_file  <- file.path(input_dir,"snv-mutation-tmb-coding.tsv")
 sbi_coding_file  <- file.path(results_dir,"splicing_index.SE.txt")
 
-indep_rna_df <- vroom(indep_rna_file) %>% filter(cohort == 'PBTA')
-indep_wgs_df <- vroom(indep_wgs_file) %>% filter(cohort == 'PBTA')
+indep_rna_df <- read_tsv(indep_rna_file) %>% 
+  filter(cohort == 'PBTA') %>%
+  dplyr::rename(Kids_First_Biospecimen_ID_RNA = Kids_First_Biospecimen_ID) %>%
+  select(Kids_First_Participant_ID, Kids_First_Biospecimen_ID_RNA)
+indep_wgs_df <- read_tsv(indep_wgs_file) %>% 
+  filter(cohort == 'PBTA') %>%
+  dplyr::rename(Kids_First_Biospecimen_ID_DNA = Kids_First_Biospecimen_ID) %>%
+  select(Kids_First_Participant_ID, Kids_First_Biospecimen_ID_DNA)
 
 ## filter for samples that have both RNA and WGS
-indep_sample_wgs_rna <- inner_join(indep_wgs_df,indep_rna_df, by='Kids_First_Participant_ID')
+indep_sample_wgs_rna <- inner_join(indep_wgs_df, indep_rna_df)
 
 ## get tmb file (source: open-pedcan)
-tmb_coding_df  <-  vroom(tmb_coding_file)  %>% 
-  mutate(Kids_First_Biospecimen_ID=Tumor_Sample_Barcode)
+tmb_coding_df  <-  read_tsv(tmb_coding_file)  %>% 
+  dplyr::rename(Kids_First_Biospecimen_ID_DNA = Tumor_Sample_Barcode) %>%
+  select(-experimental_strategy) %>%
+  right_join(indep_wgs_df) %>%
+  mutate(mutation_status = case_when(tmb <10 ~ "Normal",
+                                     tmb >=10 & tmb < 100 ~ "Hypermutant",
+                                     tmb >=100 ~ "Ultra-hypermutant"))
 
-sbi_coding_df  <-  vroom(sbi_coding_file) %>% 
-  mutate(Kids_First_Biospecimen_ID=Sample) 
-
-## subset histology file for indep/primary samples 
-clin_file = file.path(data_dir, "histologies.tsv")
-clin_df  <-  vroom(clin_file) %>% filter(cohort == 'PBTA') %>% 
-  inner_join(indep_sample_wgs_rna, by='Kids_First_Participant_ID')
-
-sbi_ids_clin <- clin_df %>% inner_join(sbi_coding_df, by="Kids_First_Biospecimen_ID") 
-tmb_ids_clin <- clin_df %>% inner_join(tmb_coding_df, by="Kids_First_Biospecimen_ID")
+sbi_coding_df  <-  read_tsv(sbi_coding_file) %>% 
+  dplyr::rename(Kids_First_Biospecimen_ID_RNA = Sample) %>%
+  select(-Histology) %>%
+  right_join(indep_rna_df)
 
 ## intersect tmb values with SBI tumors
-sbi_vs_tmb_innerjoin_df <- inner_join(sbi_ids_clin,tmb_ids_clin, by="Kids_First_Participant_ID") 
+sbi_vs_tmb_innerjoin_df <- tmb_coding_df %>%
+  inner_join(sbi_coding_df, by="Kids_First_Participant_ID") 
 
 ## identify samples by high vs low SBI tumors
-quartiles_sbi <- quantile(sbi_vs_tmb_innerjoin_df$SI, probs=c(.25, .75), na.rm = FALSE)
+quartiles_sbi <- quantile(sbi_vs_tmb_innerjoin_df$SI, probs=c(.25, .75), na.rm = TRUE)
 lower_sbi <- quartiles_sbi[1]
 upper_sbi <- quartiles_sbi[2]
-high_sbi_df <- dplyr::filter(sbi_vs_tmb_innerjoin_df, SI > upper_sbi) %>% dplyr::mutate(SBI_level="High")
-low_sbi_df  <- dplyr::filter(sbi_vs_tmb_innerjoin_df, SI < lower_sbi) %>% dplyr::mutate(SBI_level="Low")
-high_vs_low_df <- rbind(low_sbi_df,high_sbi_df)
 
-## identify samples by high vs low TMB tumors
-quartiles_tmb <- quantile(sbi_vs_tmb_innerjoin_df$tmb, probs=c(.25, .75), na.rm = FALSE)
-lower_tmb <- quartiles_tmb[1]
-upper_tmb <- quartiles_tmb[2]
-high_tmb_df <- dplyr::filter(sbi_vs_tmb_innerjoin_df, tmb > upper_tmb) %>% dplyr::mutate(TMB_level="High")
-low_tmb_df  <- dplyr::filter(sbi_vs_tmb_innerjoin_df, tmb < lower_tmb) %>% dplyr::mutate(TMB_level="Low")
-high_vs_low_TMB_df <- rbind(low_tmb_df,high_tmb_df)
-
+high_vs_low_df <- sbi_vs_tmb_innerjoin_df %>%
+  mutate(SBI_level = case_when(SI > upper_sbi ~ "High",
+                               SI < lower_sbi ~ "Low")) %>%
+  filter(!is.na(SBI_level),
+         !is.na(mutation_status))
+high_vs_low_df$mutation_status <- factor(high_vs_low_df$mutation_status, levels = c("Normal", "Hypermutant", "Ultra-hypermutant"))
+high_vs_low_df$SBI_level <- factor(high_vs_low_df$SBI_level, levels = c("Low", "High"))
 
 ## remove hyper/ultra-mutant samples
-high_vs_low_hyper_rem_df <- high_vs_low_df %>% dplyr::filter(tmb < 10)
+high_vs_low_hyper_rem_df <- high_vs_low_df %>% 
+  dplyr::filter(mutation_status == "Normal")
+
+ggplot(high_vs_low_df,aes(SBI_level,log10(tmb))) +  
+#  geom_boxplot(outlier.size = 0, size = 0.5, color = "black", alpha = 0,
+               # remove whiskers
+ #              coef = 0) +
+  ggforce::geom_sina(aes(color = SBI_level, alpha = 0.4), pch = 16, size = 4, method="density") +
+  geom_boxplot(outlier.shape = NA, color = "black", size = 0.5, coef = 0, aes(alpha = 0.4)) +
+  stat_compare_means() + 
+  facet_wrap("mutation_status") +
+  scale_color_manual(name = "SBI_level", values = c(High = "#0C7BDC", Low = "#FFC20A")) + 
+  theme_Publication() + 
+  labs(y="log10 (TMB)", x="Splicing Burden Level") + 
+  ylim(c(-3,3)) +
+  theme(legend.position="none")
+
+
 ggplot(high_vs_low_hyper_rem_df,aes(SBI_level,log10(tmb)) ) +  
   geom_violin(aes(SBI_level)) +
-  ggforce::geom_sina(aes(color = SBI_level), size = 2,method="density") +
+  ggforce::geom_sina(aes(color = SBI_level), size = 2, method="density") +
   stat_compare_means() + 
   scale_color_manual(name = "SBI_level", values = c(High = "#0C7BDC", Low = "#FFC20A")) + 
   theme_Publication() + labs(y="log10 (TMB)", x="Splicing Burden Level") + 
