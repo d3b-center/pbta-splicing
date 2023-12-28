0a3835e6e030b30631d5bde6167f52365ea8ac65
jharenza <jharenza@gmail.com>
2023-12-28 20:24:18 +0000

    add more pathways, export kinases affected

--- analyses/splicing_events_functional_sites/04-plot_splicing_across_functional_sites.R
+++ analyses/splicing_events_functional_sites/04-plot_splicing_across_functional_sites.R
@@ -7,7 +7,7 @@
 ################################################################################
 
 suppressPackageStartupMessages({
-  library("ggstatsplot")
+  library("ggpubr")
   library("vroom")
   library("ggplot2")
   library("dplyr")
@@ -27,7 +27,6 @@ suppressPackageStartupMessages({
 root_dir <- rprojroot::find_root(rprojroot::has_dir(".git"))
 data_dir <- file.path(root_dir, "data")
 analysis_dir <- file.path(root_dir, "analyses", "splicing_events_functional_sites")
-
 input_dir   <- file.path(analysis_dir, "input")
 results_dir <- file.path(analysis_dir, "results")
 
@@ -44,6 +43,9 @@ source(file.path(figures_dir, "theme_for_plots.R"))
 ## output files for final plots
 file_dpsi_plot <- file.path(analysis_dir, "plots", "dPSI_across_functional_sites.HGG.pdf")
 file_dpsi_kinase_plot <- file.path(analysis_dir, "plots", "dPSI_across_functional_sites_kinase.HGG.pdf")
+ora_dotplot_path <- file.path(plots_dir,"kinases-ora-plot.pdf")
+kinases_functional_sites_skipped = file.path(results_dir,"kinases-functional_sites_skipped.txt")
+kinases_functional_sites_incl = file.path(results_dir,"kinases-functional_sites_included.txt")
 
 ## retrieve psi values from tables
 file_psi_pos_func <-  file.path(results_dir,"splicing_events.total.HGG.pos.intersectUnip.ggplot.txt")
@@ -69,11 +71,9 @@ plot_dsp <-  ggplot(psi_comb,aes(Uniprot, dPSI*100) ) +
   geom_boxplot(outlier.shape = NA, color = "black", size = 0.5, coef = 0, aes(alpha = 0.4)) +
   facet_wrap("Preference") +
   stat_compare_means() + 
-
-  #ggforce::geom_sina(aes(color = Preference), size = 2,method="density") +
   scale_color_manual(name = "Preference", values = c(Skipping = "#0C7BDC", Inclusion = "#FFC20A"))  + 
   theme_Publication() + 
-  labs(y=expression('Percent Spliced In'), x= "Uniprot-defined Functional Site") + 
+  labs(y="Percent Spliced In (PSI)", x= "Uniprot-defined Functional Site") + 
   theme(legend.position="none")
 
 # Save plot as PDF
@@ -81,7 +81,6 @@ pdf(file_dpsi_plot,
     width = 10, height = 4)
 plot_dsp
 dev.off()
-
 
 # kinase gene list
 known_kinase_file <- file.path(input_dir,'kinase_known.txt')
@@ -98,8 +97,7 @@ kinase_dpsi_plot <- ggplot(psi_unip_kinase,aes(Preference,dPSI*100) ) +
   geom_label_repel(box.padding = 0.5, min.segment.length = 0.5,max.overlaps =Inf, aes(label = gene), data=psi_unip_kinase %>% subset(gene %in% c("CLK1")), size=2) +
   scale_color_manual(name = "Preference", values = c(Skipping = "#0C7BDC", Inclusion = "#FFC20A")) + 
   theme_Publication() +
-  labs(y=expression('Percent Spliced In')) + 
-  
+  labs(y="Percent Spliced In (PSI)") + 
   theme(legend.position="none")
 
 pdf(file_dpsi_kinase_plot, 
@@ -108,87 +106,68 @@ kinase_dpsi_plot
 dev.off()
 
 ## over-repr analysis for kinases
-# plot output path
-ora_dotplot_path = file.path(plots_dir,"kinases-ora-plot.pdf")
-
 # get gene sets relevant to H. sapiens
 hs_msigdb_df <- msigdbr(species = "Homo sapiens")
+msigdbr_collections() %>%
+  print (n = 100)
+
 
 ## filter for kegg pathways that are included in the curated gene sets
-hs_kegg_df <- hs_msigdb_df %>%
-  dplyr::filter(gs_cat == "C2", gs_subcat == "CP:KEGG"
-  )
-
-hs_hm_df <- hs_msigdb_df %>%
-  dplyr::filter(gs_cat == "H"  )
+pathway_df <- hs_msigdb_df %>%
+  dplyr::filter(gs_cat %in% c("H", "C6") | gs_subcat %in% c("CP:KEGG", "CP:BIOCARTA", "TFT:GTRD"))
 
 ## skipping vs incl 
-kinase_skip_pref <- psi_unip_kinase %>% dplyr::filter(Preference=='Skipping') 
-kinase_incl_pref <- psi_unip_kinase %>% dplyr::filter(Preference=='Inclusion') 
-
-## hallmark gene set
-ora_incl_hm_results <- enricher(
-  gene = kinase_incl_pref$gene, # A vector of your genes of interest
-  pvalueCutoff = 0.05, 
-  pAdjustMethod = "BH", 
-  TERM2GENE = dplyr::select(
-    hs_hm_df,
-    gs_name,
-    human_gene_symbol
-  )
-)
-
-ora_skip_hm_results <- enricher(
-  gene = kinase_skip_pref$gene, # A vector of your genes of interest
-  pvalueCutoff = 0.05, 
-  pAdjustMethod = "BH", 
-  TERM2GENE = dplyr::select(
-    hs_hm_df,
-    gs_name,
-    human_gene_symbol
-  )
-)
+kinase_skip_pref <- psi_unip_kinase %>% 
+  dplyr::filter(Preference=='Skipping') 
+kinase_incl_pref <- psi_unip_kinase %>% 
+  dplyr::filter(Preference=='Inclusion') 
 
 ##kegg gene set
-ora_incl_kegg_results <- enricher(
-  gene = kinase_incl_pref$gene, # A vector of your genes of interest
+ora_incl_results <- enricher(
+  gene = unique(kinase_incl_pref$gene), # A vector of your genes of interest
   pvalueCutoff = 0.05, 
   pAdjustMethod = "BH", 
   TERM2GENE = dplyr::select(
-    hs_kegg_df,
+    pathway_df,
     gs_name,
     human_gene_symbol
   )
 )
 
-ora_skip_kegg_results <- enricher(
-  gene = kinase_skip_pref$gene, # A vector of your genes of interest
+ora_skip_results <- enricher(
+  gene = unique(kinase_skip_pref$gene), # A vector of your genes of interest
   pvalueCutoff = 0.05, 
   pAdjustMethod = "BH", 
   TERM2GENE = dplyr::select(
-    hs_kegg_df,
+    pathway_df,
     gs_name,
     human_gene_symbol
   )
 )
 
-enrich_skip_hm_plot <- enrichplot::dotplot(ora_skip_hm_results) + 
-  theme_Publication() 
-enrich_incl_hm_plot <- enrichplot::dotplot(ora_incl_hm_results) + 
-  theme_Publication() 
 
-enrich_skip_kegg_plot <- enrichplot::dotplot(ora_skip_kegg_results) + 
-  theme_Publication() 
-enrich_incl_kegg_plot <- enrichplot::dotplot(ora_incl_kegg_results) + 
-  theme_Publication() 
+enrich_skip_plot <- enrichplot::dotplot(ora_skip_results, showCategory = 15) + 
+  theme_Publication() +
+  scale_color_gradient(name = "Adjusted p-value", 
+                       low = "orange", high = "#0C7BDC") +  # Modify color range
+  labs(color = "B-H adj p-value")  # Modify legend title 
+enrich_incl_plot <- enrichplot::dotplot(ora_incl_results, showCategory = 15) +
+  theme_Publication() +
+  scale_color_gradient(name = "Adjusted p-value", 
+                       low = "orange", high = "#0C7BDC") +  # Modify color range
+  labs(color = "B-H adj p-value")  # Modify legend title
 
 ## since kegg pathways have and show more enrichment, we will save those
-plot_pathways_kegg <- plot_grid(enrich_skip_kegg_plot,enrich_incl_kegg_plot,align="hv",labels = c('Skipping', 'Inclusion'))
-plot_pathways_kegg
+plot_pathways <- plot_grid(enrich_skip_plot,enrich_incl_plot,align="hv",
+                                labels = c('Exon Skipping', 'Exon Inclusion'))
+plot_pathways
 
-## save ORA dotplot as tiff
+## save ORA dotplot
 ggplot2::ggsave(ora_dotplot_path,
-                width=20,
-                height=6,
+                width=17,
+                height=7,
                 device="pdf")
 
+## write kinase results for table
+write_lines(sort(unique(kinase_skip_pref$gene)), kinases_functional_sites_skipped)
+write_lines(sort(unique(kinase_incl_pref$gene)), kinases_functional_sites_incl)
