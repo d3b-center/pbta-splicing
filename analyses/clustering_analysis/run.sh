17168335ec80cbad4954a2eca66347a9465b0513
Ammar S. Naqvi <naqvia@email.chop.edu>
2023-12-28 10:33:30 -0500

    renamed scripts and modified run.sh

--- analyses/clustering_analysis/run.sh
+++ analyses/clustering_analysis/run.sh
@@ -1,5 +1,11 @@
-# 0) run script for optimal clustering
-Rscript code/00-optimal-clustering.R \
+# 0) scripts to create matrix and input
+# pre-process and create neccessary input for clustering
+perl code/00-create_matrix_of_PSI_SE_gene.pl ../../data/histologies.tsv ../../data/splice-events-rmats.tsv.gz ../../data/independent-specimens.rnaseqpanel.primary.tsv
+Rscript --vanilla code/01-convert_to_rds.R
+Rscript --vanilla code/02-create-inputs.R
+
+# 1) run script for optimal clustering
+Rscript code/03-optimal-clustering.R \
 --input_mat "input/non_expr_pan_cancer_splice_subset.rds" \
 --cluster_algorithm "hc, km, pam" \
 --cluster_distance "pearson, spearman, euclidean, manhattan, binary, maximum, canberra, minkowski" \
@@ -12,7 +18,7 @@ Rscript code/00-optimal-clustering.R \
 # 1) PBTA splicing data
 # get ccp clustering output for a specific combination of distance + algorithm + % variable genes
 # for the splicing dataset, we chose pam, pearson at 0% (based on Ammar's manual inspection)
-Rscript code/01-get-clustering-output.R \
+Rscript code/04-get-clustering-output.R \
 --input_mat "input/non_expr_pan_cancer_splice_subset.rds" \
 --data_type "non_expr" \
 --var_genes "0" \
@@ -22,7 +28,7 @@ Rscript code/01-get-clustering-output.R \
 
 # get differential genes per cluster and perform pre-ranked gsea using those genes
 # this was done for k = 3 with pam  + pearson + 0% genes
-Rscript code/02-diff-genes-per-clusters.R \
+Rscript code/05-diff-genes-per-clusters.R \
 --input_mat "output/ccp_output/non_expr_pan_cancer_splice_subset_pam_spearman_0_matrix.rds" \
 --cluster_output "output/ccp_output/non_expr_pan_cancer_splice_subset_pam_spearman_0_ccp.rds" \
 --n_cluster "8" \
@@ -32,7 +38,7 @@ Rscript code/02-diff-genes-per-clusters.R \
 
 # get differential pathways per cluster using GSVA
 # this was done for k = 3 with pam  + pearson + 0% genes
-Rscript code/03-diff-pathways-per-clusters.R \
+Rscript code/06-diff-pathways-per-clusters.R \
 --input_mat "output/ccp_output/non_expr_pan_cancer_splice_subset_pam_spearman_0_matrix.rds" \
 --input_clin "../../data/histologies.tsv" \
 --cluster_output "output/ccp_output/non_expr_pan_cancer_splice_subset_pam_spearman_0_ccp.rds" \
@@ -42,7 +48,7 @@ Rscript code/03-diff-pathways-per-clusters.R \
 --output_dir "output/diff_pathways"
 
 # get heatmap of the CCP matrix
-Rscript code/04-plot-clustering-heatmap.R \
+Rscript code/07-plot-clustering-heatmap.R \
 --ccp_output "output/ccp_output/non_expr_pan_cancer_splice_subset_pam_spearman_0_ccp.rds" \
 --input_clin "../../data/histologies.tsv" \
 --n_cluster "8" \
@@ -52,7 +58,7 @@ Rscript code/04-plot-clustering-heatmap.R \
 # get clustering output with same clustering parameters used for pbta splicing dataset
 # we will not be using the CCP clustering output but only the filtered/normalized data matrix for downstream plotting
 # we will be using the clustering output obtained from the splicing dataset
-Rscript code/01-get-clustering-output.R \
+Rscript code/04-get-clustering-output.R \
 --input_mat "input/raw_counts_pbta_subset.rds" \
 --data_type "raw_counts" \
 --var_genes "0" \
@@ -62,7 +68,7 @@ Rscript code/01-get-clustering-output.R \
 
 # get differential genes per cluster and perform pre-ranked gsea using those genes
 # this was done for k = 3 with pam  + pearson + 0% genes
-Rscript code/02-diff-genes-per-clusters.R \
+Rscript code/05-diff-genes-per-clusters.R \
 --input_mat "output/ccp_output/raw_counts_pbta_subset_pam_spearman_0_matrix.rds" \
 --cluster_output "output/ccp_output/non_expr_pan_cancer_splice_subset_pam_spearman_0_ccp.rds" \
 --n_cluster "8" \
@@ -72,7 +78,7 @@ Rscript code/02-diff-genes-per-clusters.R \
 
 # get differential pathways per cluster using GSVA
 # this was done for k = 3 with pam  + pearson + 0% genes
-Rscript code/03-diff-pathways-per-clusters.R \
+Rscript code/06-diff-pathways-per-clusters.R \
 --input_mat "output/ccp_output/raw_counts_pbta_subset_pam_spearman_0_matrix.rds" \
 --input_clin "../../data/histologies.tsv" \
 --cluster_output "output/ccp_output/non_expr_pan_cancer_splice_subset_pam_spearman_0_ccp.rds" \
@@ -82,7 +88,7 @@ Rscript code/03-diff-pathways-per-clusters.R \
 --output_dir "output/diff_pathways"
 
 ##plot cluster members categorized by SBI high vs low
-Rscript code/05-plot-sbi_with_cluster-mem.R
+Rscript code/07-plot-sbi_with_cluster-mem.R
 
 ## plot histologies across clusters
-Rscript code/06-plot-histology-distr-across-clusters.R
+Rscript code/08-plot-histology-distr-across-clusters.R
