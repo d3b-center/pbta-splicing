3e89363be864527eda49b8ea0753fe2bd0498b73
Ammar S. Naqvi <naqvia@email.chop.edu>
2023-12-29 13:58:30 -0500

    changed input file and run module

---
+++ analyses/CLK1-splicing_correlations/run_module.sh
@@ -0,0 +1,9 @@
+## plot HGGs with high vs low exon 4
+Rscript --vanilla 01-plot_highExon4_vs_lowExon4_and_SBI.R
+
+## plot correlations of splicing vs expr
+Rscript --vanilla 02-plot_splicing_vs_expr.R
+Rscript --vanilla 03-plot_SR-phosp_vs_CLK1-RNA.R
+
+## plot CLK1 stacked barplots
+Rscript --vanilla 04-CLK1_PSI_plots.R
