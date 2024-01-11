ab21b060ca065cfa9ff162b7b33f24e700f4767c
Ammar S. Naqvi <naqvia@email.chop.edu>
2024-01-11 12:07:43 -0500

    modified code for v7

--- analyses/histology-specific-splicing/run_module.sh
+++ analyses/histology-specific-splicing/run_module.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 ## histology and rmats file
-hist_file="../splicing_index/results/histologies-plot-group.tsv"
+hist_file="../../data/histologies.tsv"
 rmats_file="../../data/splice-events-rmats.tsv.gz"
-indep_samples_plus="../../data/independent-specimens.rnaseqpanel.primary-plus.tsv"
+indep_samples="../../data/independent-specimens.rnaseqpanel.primary.tsv"
 
 ## 2% histology specific splicing based on splicing index computations for SE
-perl 01-generate_hist_spec_events_tab.pl $hist_file $rmats_file $indep_samples_plus
+perl 01-generate_hist_spec_events_tab.pl $hist_file $rmats_file $indep_samples
 Rscript --vanilla 02-plot_histology-specific_splicing_events.R
 
