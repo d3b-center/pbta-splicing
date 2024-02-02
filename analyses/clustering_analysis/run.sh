3476130862608e8391567bfa3b693fa988247449
Ammar S. Naqvi <naqvia@email.chop.edu>
2024-02-02 14:31:46 -0500

    modified params for run script

--- analyses/clustering_analysis/run.sh
+++ analyses/clustering_analysis/run.sh
@@ -40,7 +40,7 @@ Rscript code/05-diff-genes-per-clusters.R \
 # this was done for k = 3 with pam  + pearson + 0% genes
 Rscript code/06-diff-pathways-per-clusters.R \
 --input_mat "output/ccp_output/non_expr_pan_cancer_splice_subset_pam_canberra_0_matrix.rds" \
---input_clin "input/histologies-plot-group.tsv" \
+--input_clin "../cohort_summary/results/histologies-plot-group.tsv" \
 --cluster_output "output/ccp_output/non_expr_pan_cancer_splice_subset_pam_canberra_0_ccp.rds" \
 --n_cluster "13" \
 --gene_set "input/kegg_geneset_mrna.rds" \
@@ -50,45 +50,12 @@ Rscript code/06-diff-pathways-per-clusters.R \
 # get heatmap of the CCP matrix
 Rscript code/07-plot-clustering-heatmap.R \
 --ccp_output "output/ccp_output/non_expr_pan_cancer_splice_subset_pam_canberra_0_ccp.rds" \
---input_clin "input/histologies-plot-group.tsv" \
---n_cluster "8" \
+--input_clin "../cohort_summary/results/histologies-plot-group.tsv" \
+--n_cluster "13" \
 --prefix "non_expr_pan_cancer_splice_subset"
 
-# 2) PBTA mRNA expression data (subsetted to samples in the splicing data)
-# get clustering output with same clustering parameters used for pbta splicing dataset
-# we will not be using the CCP clustering output but only the filtered/normalized data matrix for downstream plotting
-# we will be using the clustering output obtained from the splicing dataset
-Rscript code/04-get-clustering-output.R \
---input_mat "input/raw_counts_pbta_subset.rds" \
---data_type "raw_counts" \
---var_genes "0" \
---cluster_algorithm "pam" \
---cluster_distance "canberra" \
---prefix "raw_counts_pbta_subset"
-
-# get differential genes per cluster and perform pre-ranked gsea using those genes
-# this was done for k = 3 with pam  + pearson + 0% genes
-Rscript code/05-diff-genes-per-clusters.R \
---input_mat "output/ccp_output/raw_counts_pbta_subset_pam_canberra_0_matrix.rds" \
---cluster_output "output/ccp_output/non_expr_pan_cancer_splice_subset_pam_canberra_0_ccp.rds" \
---n_cluster "13" \
---gene_set "input/kegg_geneset_mrna.rds" \
---prefix "raw_counts_pbta_subset_pam_spearman_0" \
---output_dir "output/diff_genes"
-
-# get differential pathways per cluster using GSVA
-# this was done for k = 3 with pam  + pearson + 0% genes
-Rscript code/06-diff-pathways-per-clusters.R \
---input_mat "output/ccp_output/raw_counts_pbta_subset_pam_canberra_0_matrix.rds" \
---input_clin "input/histologies-plot-group.tsv" \
---cluster_output "output/ccp_output/non_expr_pan_cancer_splice_subset_pam_canberra_0_ccp.rds" \
---n_cluster "13" \
---gene_set "input/kegg_geneset_mrna.rds" \
---prefix "raw_counts_pbta_subset_pam_canberra_0" \
---output_dir "output/diff_pathways"
-
 ##plot cluster members categorized by SBI high vs low
-#Rscript code/07-plot-sbi_with_cluster-mem.R
+Rscript code/08-plot-sbi_with_cluster-mem.R
 
 ## plot histologies across clusters
-#Rscript code/08-plot-histology-distr-across-clusters.R
+Rscript code/09-plot-histology-distr-across-clusters.R
