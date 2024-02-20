f8b7c9f90b4cd007982e2c018a1b6b56df8e37ee
Ammar S. Naqvi <naqvia@email.chop.edu>
2024-02-20 10:15:33 -0500

    deseq2 results modified

--- analyses/make-suppl-tables.R
+++ analyses/make-suppl-tables.R
@@ -152,8 +152,8 @@ write.xlsx(list_s2_table,
 
 ## Table 3 DeSeq 2 results comparing high vs low SBI HGG tumors
 ## sheet 1, exon inclusion splicing
-deseq_df <- vroom(deseq2_sf_file)
-
+deseq_df <- vroom(deseq2_sf_file) %>%
+  select(-Significant)
 
 # Combine and output
 list_s3_table <- list(deseq2=deseq_df)
