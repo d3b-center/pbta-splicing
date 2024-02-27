890f810d34014e843f049927775d5a2eca6e2df7
Ammar S. Naqvi <naqvia@email.chop.edu>
2024-02-27 11:13:23 -0500

    initial code for co-occ

--- analyses/co-occurrence/01-co-occurrence-interactions.R
+++ analyses/co-occurrence/01-co-occurrence-interactions.R
@@ -1,3 +1,4 @@
+
 ################################################################################
 # 01-co-occurence-interactions.R
 # written by Ammar Naqvi, Jo Lynne Rokita
@@ -25,6 +26,9 @@ input_dir   <- file.path(analysis_dir, "input")
 plots_dir   <- file.path(analysis_dir, "plots")
 results_dir   <- file.path(analysis_dir, "results")
 
+# Load functions
+source(file.path(analysis_dir, "util/cooccur_functions.R"))
+
 ## check and create plots/results dir
 if(!dir.exists(plots_dir)){
   dir.create(plots_dir, recursive=TRUE)
@@ -35,11 +39,15 @@ if(!dir.exists(results_dir)){
 }
 
 ## input files
-maf_file <- file.path(data_dir,"snv-consensus-plus-hotspots.maf.tsv.gz")
+maf_df <- data.table::fread(file.path(data_dir,"snv-consensus-plus-hotspots.maf.tsv.gz"), data.table = FALSE)
+
 clin_file <- file.path(data_dir, "histologies.tsv")
 indep_rna_file <- file.path(data_dir, "independent-specimens.rnaseqpanel.primary.tsv")
 indep_wgs_file <- file.path(data_dir, "independent-specimens.wgswxspanel.primary.prefer.wgs.tsv")
 diff_psi_file <- file.path(root_dir, "analyses/splicing_events_functional_sites/results/splice_events.diff.SE.HGG.txt")
+goi_file <- file.path(input_dir,"oncoprint-goi-lists-OpenPedCan-gencode-v39.csv")
+
+goi_df <- vroom(goi_file) %>% select(HGAT) %>% rename(gene=HGAT)
 
 indep_rna_df <- vroom(indep_rna_file)  %>% 
   dplyr::filter(cohort == 'PBTA') 
@@ -69,116 +77,129 @@ histologies_df <- vroom(clin_file)  %>%
 splice_df <-  vroom(diff_psi_file) %>%
   dplyr::rename('RNA_id'=Sample) %>% 
   inner_join(histologies_df, by='RNA_id', relationship = "many-to-many") %>%
-  dplyr::mutate(Hugo_Symbol=str_match(`Splice ID`, "(\\w+)\\:")[, 2]) 
+  dplyr::mutate(gene=str_match(`Splice ID`, "(\\w+)\\:")[, 2]) %>%
+  dplyr::filter(`Splice ID`=='CLK1:200860125-200860215_200859679-200859746_200861237-200861466')
 
-splice_maf_df <- splice_df %>%
-  dplyr::rename('Tumor_Sample_Barcode'=Kids_First_Biospecimen_ID) %>%
-  dplyr::select(Tumor_Sample_Barcode,Hugo_Symbol) %>%
-  mutate(Variant_Classification='Splicing', Variant_Type='Other') %>% 
-  unique() #%>% 
-  #dplyr::slice(1:10000) ## testing puroses
+maf_df <- maf_df %>% 
+  dplyr::mutate(vaf = t_alt_count / (t_ref_count + t_alt_count))
+
 
 ## filter maf table for samples with RNA splicing + HGGs + midline + req'd cols
-maf_df <- vroom(maf_file) %>% 
+maf_df <- maf_df  %>% 
   dplyr::mutate(vaf = t_alt_count / (t_ref_count + t_alt_count))
 
 ## select required columns only
-maf_fil_df <- maf_df %>%
+maf_df <- maf_df %>%
   inner_join(histologies_df, by= c("Tumor_Sample_Barcode" ="DNA_id"),relationship = "many-to-many") %>% 
   dplyr::mutate('Tumor_Sample_Barcode'=Kids_First_Participant_ID) %>% 
   dplyr::filter(vaf > 0.05) %>% 
+  dplyr::filter(Hugo_Symbol %in% goi_df$gene) %>% 
   dplyr::select(
-    Hugo_Symbol,
-    Entrez_Gene_Id,
-    Chromosome,
-    Start_Position,
-    End_Position,
-    Strand,
-    Variant_Classification,
-    Variant_Type,
-    Reference_Allele,
-    Tumor_Seq_Allele1,
-    Tumor_Seq_Allele2,
-    Tumor_Sample_Barcode,
-    t_depth,
-    t_ref_count,
-    t_alt_count,
-    Consequence
+    "Hugo_Symbol", 
+    "Chromosome", 
+    "Start_Position", 
+    "End_Position", 
+    "Reference_Allele", 
+    "Tumor_Seq_Allele2", 
+    "Variant_Classification", 
+    "Variant_Type",
+    "Tumor_Sample_Barcode"
   ) 
 
-## filter for only those genes that show diff splicing 
-maf_fil_w_dsg <- maf_fil_df %>% 
-  #left_join(splice_maf_df, by = c("Hugo_Symbol","gene_to_check")) %>% 
-  left_join(splice_maf_df, by = "Hugo_Symbol") %>%
-  dplyr::mutate(Hugo_Symbol=paste0(Hugo_Symbol,"_mut")) 
+## cnv
+# ## read in cnv (OpenPedCan v11)
+# cnv_df <- readr::read_tsv(file.path(data_dir, "consensus_wgs_plus_cnvkit_wxs_plus_freec_tumor_only.tsv.gz")) %>%
+#   # filter for HGGs and gene of interest
+#   filter(biospecimen_id %in% histologies_df$Kids_First_Biospecimen_ID) %>%
+#   mutate(Hugo_Symbol = gene_symbol,
+#          Tumor_Sample_Barcode = biospecimen_id,
+#          Variant_Classification = status) %>%
+#   dplyr::filter(Hugo_Symbol %in% goi_df$gene) %>% 
+#   select(Hugo_Symbol, Tumor_Sample_Barcode, Variant_Classification) %>%
+#   # mutate loss and amplification to Del and Amp to fit Maftools format
+#   dplyr::mutate(Variant_Classification = dplyr::case_when(Variant_Classification == "deep deletion" ~ "Del",
+#                                                           Variant_Classification == "amplification" ~ "Amp",
+#                                                           TRUE ~ as.character(Variant_Classification))) %>%
+#   # only keep Del and Amp calls
+#   filter(Variant_Classification %in% c("Del", "Amp")) %>%
+#   distinct()
+
+# cnv_df <- cnv_df %>%
+#   filter(!is.na(Variant_Classification))%>%
+#   distinct() %>%
+#   as.data.frame()
 
 
-splice_maf_df <- splice_maf_df %>%
-  dplyr::mutate(Hugo_Symbol=paste0(Hugo_Symbol,"_spl"))
-    
-    
-## rename histologies_df Kids_First_Participant_ID to Tumor_Sample_Barcode
-histologies_df <- histologies_df %>%  
-  dplyr::rename('Tumor_Sample_Barcode'=Kids_First_Participant_ID)
-  
-## combine mutation and splicing info
-combined_maf <- bind_rows(maf_fil_w_dsg, splice_maf_df)
+collapse_snv_dat <- select(maf_df,c(Hugo_Symbol,Tumor_Sample_Barcode,Variant_Classification)) %>%
+  unique() %>%
+  filter(Variant_Classification %in% names(colors)) %>%
+  group_by(Hugo_Symbol,Tumor_Sample_Barcode) %>%
+  summarise(count = as.double(length(Variant_Classification[!is.na(Variant_Classification)])),
+            Variant_Classification=paste(Variant_Classification,collapse = ",")) 
 
-## create MAF object
-maf = read.maf(maf = combined_maf, 
-               clinicalData = histologies_df,
-               vc_nonSyn = c("Frame_Shift_Del", 
-                             "Frame_Shift_Ins", 
-                             "Splice_Site", 
-                             "Translation_Start_Site",
-                             "Nonsense_Mutation",
-                             "Nonstop_Mutation", 
-                             "In_Frame_Del",
-                             "In_Frame_Ins", 
-                             "Missense_Mutation",
-                             "Del",
-                             "Amp",
-                             "Splicing") )
+# complex heatmap
+gene_matrix<-reshape2::acast(collapse_snv_dat,
+                             Hugo_Symbol~Tumor_Sample_Barcode,value.var = "Variant_Classification")
+
+## read input
+#gene_matrix <- gene_matrix[goi_list$genes,]
+##read in input
+goi_list <- read_tsv(file.path(input_dir, "goi-mutations"), col_names = "genes")
+germline <- read_tsv(file.path(input_dir, "germline_variants_meta_format.tsv"))
+
+# read in telomerase scores
+splice_CLK1_df <-splice_df %>%
+  rename(PSI = dPSI) %>%
+  select(Kids_First_Participant_ID, PSI)
 
 
-## define colors
-colors = c("Missense_Mutation"      = "#35978f",
-           "Nonsense_Mutation"      = "#000000",
-           "Frame_Shift_Del"        = "#56B4E9",
-           "Frame_Shift_Ins"        = "#FFBBFF",
-           "Splice_Site"            = "#F0E442",
-           "Translation_Start_Site" = "#191970",
-           "Nonstop_Mutation"       = "#545454",
-           "In_Frame_Del"           = "#CAE1FF",
-           "In_Frame_Ins"           = "#FFE4E1",
-           "Multi_Hit"              = "#f46d43",
-           "Del"                    = "#0072B2",
-           "Amp"                    = "#c51b7d",
-           "Splicing"               = "green")
+# mutate the hgat dataframe for plotting
+histologies_df <- histologies_df %>%
+  inner_join(splice_CLK1_df, by = "Kids_First_Participant_ID")
 
-variant_labels = c("Missense Mutation",
-                   "Nonsense Mutation",
-                   "Frameshift Deletion",
-                   "Frameshift Insertion",
-                   "Splice Site Mutation",
-                   "Translation Start Site",
-                   "Nonstop Mutation",
-                   "Inframe Deletion",
-                   "Inframe Insertion",
-                   "Multi-Hit",
-                   "Del",
-                   "Amp",
-                   "Splicing")
 
-## oncoplot
-oncoplot(maf,annotationFontSize = 1, gene_mar = 7, barcode_mar = 6,
-         sortByAnnotation = T, fontSize = 1, legendFontSize = 1,
-         showTitle = F, logColBar = T, colors = colors, removeNonMutated= T,clinicalFeatures = c("molecular_subtype"))
+ha = HeatmapAnnotation(name = "annotation", df = histologies_df[,c("reported_gender","PSI")],
+                      col=list(
+                        "reported_gender" = c("Male" = "#56B4E9",
+                                  "Female" = "#CC79A7"),
+                        "PSI" = colorRamp2(c(0, .5, 1.0), c("whitesmoke", "#CAE1FF","#0072B2")),
+                       annotation_name_side = "right", 
+                       annotation_name_gp = gpar(fontsize = 9),
+                       na_col = "whitesmoke") )
 
-## show co-occurences/mutually exclusive
-somaticInteractions(maf = maf, top = 25, pvalue = c(0.05, 0.1))
-co_occurrence_df <- somaticInteractions(maf = maf) %>% 
-  dplyr::filter(pAdj < 0.05)
+## color for barplot
+source(file.path(input_dir, "mutation-colors.R"))
 
-## write_tsv
-readr::write_tsv(co_occurrence_df, file.path(results_dir, "co-occurence-sign.tsv"))
+
+col = colors
+df = histologies_df[,c("Kids_First_Participant_ID", "reported_gender","PSI")]
+
+colorder <- df$Kids_First_Participant_ID
+
+oncoPrint(gene_matrix, get_type = function(x) strsplit(x, ",")[[1]],
+          column_names_gp = gpar(fontsize = 9), show_column_names = F, #show_row_barplot = F,
+          alter_fun = list(
+            background = function(x, y, w, h) grid.rect(x, y, w, h, gp = gpar(fill = "whitesmoke",col="whitesmoke")),
+            Missense_Mutation = function(x, y, w, h) grid.rect(x, y, w*0.85, h*0.85, gp = gpar(fill = unname(col["Missense_Mutation"]),col = NA)),
+            Nonsense_Mutation = function(x, y, w, h) grid.rect(x, y, w*0.85, h*0.85, gp = gpar(fill = unname(col["Nonsense_Mutation"]),col = NA)),
+            Frame_Shift_Del = function(x, y, w, h) grid.rect(x, y, w*0.85, h*0.85, gp = gpar(fill = unname(col["Frame_Shift_Del"]), col = NA)),
+            Frame_Shift_Ins = function(x, y, w, h) grid.rect(x, y, w*0.85, h*0.85, gp = gpar(fill = unname(col["Frame_Shift_Ins"]), col = NA)),
+            Splice_Site = function(x, y, w, h) grid.rect(x, y, w*0.85, h*0.85, gp = gpar(fill = unname(col["Splice_Site"]), col = NA)),
+            Translation_Start_Site = function(x, y, w, h) grid.rect(x, y, w*0.85, h*0.85, gp = gpar(fill = unname(col["Translation_Start_Site"]), col = NA)),
+            Nonstop_Mutation = function(x, y, w, h) grid.rect(x, y, w*0.85, h*0.85, gp = gpar(fill = unname(col["Nonstop_Mutation"]),col = NA)),
+            In_Frame_Del = function(x, y, w, h) grid.rect(x, y, w*0.85, h*0.85, gp = gpar(fill = unname(col["In_Frame_Del"]),col = NA)),
+            In_Frame_Ins = function(x, y, w, h) grid.rect(x, y, w*0.85, h*0.85, gp = gpar(fill = unname(col["In_Frame_Ins"]), col = NA)),
+            Stop_Codon_Ins = function(x, y, w, h) grid.rect(x, y, w*0.85, h*0.85, gp = gpar(fill = unname(col["Stop_Codon_Ins"]), col = NA)),
+            Start_Codon_Del = function(x, y, w, h) grid.rect(x, y, w*0.85, h*0.85, gp = gpar(fill = unname(col["Start_Codon_Del"]), col = NA)),
+            Fusion = function(x, y, w, h) grid.rect(x, y, w*0.85, h*0.85, gp = gpar(fill = unname(col["Fusion"]),col = NA)),
+            Multi_Hit_Fusion  = function(x, y, w, h) grid.rect(x, y, w*0.85, h*0.85, gp = gpar(fill = unname(col["Multi_Hit_Fusion"]),col = NA)),
+            Multi_Hit = function(x, y, w, h) grid.rect(x, y, w*0.75, h*0.85, gp = gpar(fill = unname(col["Multi_Hit"]), col = NA)),
+            Del = function(x, y, w, h) grid.rect(x, y, w*0.85, h*0.85, gp = gpar(fill = unname(col["Del"]), col = NA)),
+            Amp = function(x, y, w, h) grid.rect(x, y, w*0.85, h*0.85, gp = gpar(fill = unname(col["Amp"]), col = NA)),
+            `5'Flank` = function(x, y, w, h) grid.rect(x, y, w*0.85, h*0.85, gp = gpar(fill = unname(col["5'Flank"]), col = NA))),
+          col = col,
+          top_annotation = ha,
+          #bottom_annotation = ha1,
+          column_order =  colnames(gene_matrix)
+)
+                  
