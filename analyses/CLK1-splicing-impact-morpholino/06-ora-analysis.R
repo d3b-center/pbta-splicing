58734fe06bf598add0b6c0d422e4f6c82f7cfb38
Ammar S. Naqvi <naqvia@email.chop.edu>
2024-03-04 16:16:04 -0500

    Modified ORA to separate out inclusion vs skipping events

--- analyses/CLK1-splicing-impact-morpholino/06-ora-analysis.R
+++ analyses/CLK1-splicing-impact-morpholino/06-ora-analysis.R
@@ -41,32 +41,46 @@ figures_dir <- file.path(root_dir, "figures")
 source(file.path(figures_dir, "theme_for_plots.R"))
 
 ## outplut file for plot
-ora_dotplot_path <- file.path(plots_dir, "CLK1_targets_ora_dotplot.pdf")
+ora_dotplot_inc_path <- file.path(plots_dir, "CLK1_targets_ora_dotplot.incl-in-high-Ex.pdf")
+ora_dotplot_skp_path <- file.path(plots_dir, "CLK1_targets_ora_dotplot.skip-in-high-Ex.pdf")
 
 ## get gene sets relevant to H. sapiens
 hs_msigdb_df <- msigdbr(species = "Homo sapiens")
 pathway_df <- hs_msigdb_df %>%
-  dplyr::filter(gs_cat == "H" | gs_subcat %in% c("CP:KEGG", "CP:BIOCARTA"))
+  dplyr::filter(gs_cat == "H" | gs_subcat %in% c("CP:KEGG"))
 
 ## extract splicing changes 
 rmats_merged_file  <- file.path(analysis_dir,"input","morpholno.merged.rmats.tsv")
 splicing_df  <-  vroom(rmats_merged_file, comment = "#", delim="\t") %>% 
   filter(FDR < 0.05 & PValue < 0.05) 
 
-## create a background set of mis-spliced genes
-background_set <- splicing_df %>% dplyr::select(geneSymbol) %>% unique()
+
 
 ## create a genes of interest list based on strong splicing 
-genes_of_interest <- splicing_df %>% 
-                              filter( (IncLevelDifference >= .10) | (IncLevelDifference <= -.10)) %>% 
+genes_of_interest_inc <- splicing_df %>% 
+                              filter( (IncLevelDifference <= -.10)) %>% ## only get events that are high in Ex 4 (untreated) cells
                               dplyr::select(geneSymbol) %>% 
-                             unique()
+                              unique()
+
+## create a genes of interest list based on strong splicing 
+genes_of_interest_skip <- splicing_df %>% 
+  filter( (IncLevelDifference >= .10)) %>% ## only get events that are high in Ex 4 (untreated) cells
+  dplyr::select(geneSymbol) %>% 
+  unique()
+
+genes_of_interest_inc_uniq <- anti_join(genes_of_interest_inc, genes_of_interest_skip, by='geneSymbol' )
+genes_of_interest_skip_uniq <- anti_join(genes_of_interest_skip,genes_of_interest_inc, by='geneSymbol')
+
+bg_set<-  splicing_df %>% 
+  dplyr::select(geneSymbol) %>% 
+  unique()
 
 
 ## run enrichR to compute and identify significant over-repr pathways
 ora_results <- enricher(
-  gene = genes_of_interest$geneSymbol, # A vector of your genes of interest
+  gene = genes_of_interest_inc_uniq$geneSymbol, # A vector of your genes of interest
   pvalueCutoff = 0.05, 
+  #universe = bg_set$geneSymbol,
   pAdjustMethod = "BH", 
   TERM2GENE = dplyr::select(
     pathway_df,
@@ -77,7 +91,7 @@ ora_results <- enricher(
 
 ## plot enrichment using dotplot
 ora_result_df <- data.frame(ora_results@result)
-enrich_plot <- enrichplot::dotplot(ora_results) +   
+enrich_plot_incl<- enrichplot::dotplot(ora_results) +   
   theme_Publication() +
   scale_color_gradient(name = "Adjusted p-value", 
                        low = "orange", high = "#0C7BDC") +  # Modify color range
@@ -85,11 +99,36 @@ enrich_plot <- enrichplot::dotplot(ora_results) +
 
 
 ## save ORA dotplot as tiff
-ggplot2::ggsave(ora_dotplot_path,
-                plot=enrich_plot,
+ggplot2::ggsave(ora_dotplot_inc_path,
+                plot=enrich_plot_incl,
                 width=9,
                 height=5,
                 device="pdf",
                 dpi=300)
+
+## run enrichR to compute and identify significant over-repr pathways
+ora_results <- enricher(
+  gene = genes_of_interest_skip$geneSymbol, # A vector of your genes of interest
+  pvalueCutoff = 0.05, 
+  #universe = bg_set$geneSymbol,
+  pAdjustMethod = "BH", 
+  TERM2GENE = dplyr::select(
+    pathway_df,
+    gs_name,
+    human_gene_symbol
+  )
+)
+
+ora_result_df <- data.frame(ora_results@result)
+enrich_plot_skip<- enrichplot::dotplot(ora_results) +   
+  theme_Publication() +
+  scale_color_gradient(name = "Adjusted p-value", 
+                       low = "orange", high = "#0C7BDC") +  # Modify color range
+  labs(color = "B-H adj p-value")  # Modify legend title 
                 
-  
+ggplot2::ggsave(ora_dotplot_skp_path,
+                plot=enrich_plot_skip,
+                width=9,
+                height=5,
+                device="pdf",
+                dpi=300)
