1e9844f18cc9423614b0499660960cbeb9bc2117
Ammar S. Naqvi <naqvia@email.chop.edu>
2024-03-07 10:40:39 -0500

    code to intersect DES and DEX to see if there a link

---
+++ analyses/CLK1-splicing-impact-morpholino/08-intersection-dex-des.R
@@ -0,0 +1,116 @@
+################################################################################
+# 08-intersection-dex-des.R
+# Over-represenative analysis of mis-spliced genes that have splicing variants
+# impacting functional sitesmediated by CLK1
+#
+# authors: Ammar Naqvi 
+################################################################################
+
+## load libraries
+suppressPackageStartupMessages({
+  library("clusterProfiler")
+  library("msigdbr")
+  library("org.Hs.eg.db")
+  library("ggplot2")
+  library("DOSE")
+  library("vroom")
+  library("tidyverse")
+  library('ggVennDiagram')
+})
+
+# Get `magrittr` pipe
+`%>%` <- dplyr::`%>%`
+
+## set up directories
+root_dir <- rprojroot::find_root(rprojroot::has_dir(".git"))
+data_dir <- file.path(root_dir, "data")
+analysis_dir <- file.path(root_dir, "analyses", "CLK1-splicing-impact-morpholino")
+
+input_dir   <- file.path(analysis_dir, "input")
+results_dir <- file.path(analysis_dir, "results")
+plots_dir <- file.path(analysis_dir, "plots")
+
+##input files
+de_file <- file.path(results_dir,"ctrl_vs_treated.de.tsv")
+rmats_merged_file  <- file.path(data_dir,"morpholno.merged.rmats.tsv")
+
+## outplut file for plot
+ora_dotplot_func_path <- file.path(plots_dir, "CLK1_ds-dex-targets_ora_dotplot.pdf")
+venn_output_file <- file.path(plots_dir, "des-dex-venn.pdf")
+
+## extract strong splicing changes
+splicing_df  <-  vroom(rmats_merged_file, comment = "#", delim="\t") %>%
+  filter(FDR < 0.05 & PValue < 0.05)
+
+## extract strong differential splicing cases (dPSI >= |.10|)
+splicing_df_ES <- splicing_df %>% filter(IncLevelDifference  >= .10) %>% mutate(Preference="Skipping")
+splicing_df_EI <- splicing_df %>% filter(IncLevelDifference <= -.10) %>% mutate(Preference="Inclusion",
+                                                                                IncLevelDifference = abs(IncLevelDifference) )
+
+psi_comb <- rbind(splicing_df_EI,splicing_df_ES)
+
+dex_df_up  <-  vroom(de_file, comment = "#", delim="\t") %>%
+  filter(log2FoldChange > 2 & padj < 0.05) %>%
+  mutate(Preference='Up')
+
+dex_df_down  <-  vroom(de_file, comment = "#", delim="\t") %>%
+  filter(log2FoldChange < 2 & padj < 0.05) %>%
+  mutate(Preference='Up')
+
+dex_comb <- rbind(dex_df_up,dex_df_down) %>% 
+  dplyr::rename(geneSymbol=Gene_Symbol)
+  
+intersect <- inner_join(psi_comb,dex_comb, by='geneSymbol') %>%
+  dplyr::select(geneSymbol,Preference.x,Preference.y)
+
+total_events <- full_join(psi_comb,dex_comb, by='geneSymbol') %>%
+  dplyr::select(geneSymbol,Preference.x,Preference.y) %>% 
+  dplyr::select(geneSymbol) %>% 
+  unique()
+
+## vennn
+## plot venn diagram based on genes for each cluster (up/down/all)
+venn_diag<- ggVennDiagram(x=list(dex_comb$geneSymbol, psi_comb$geneSymbol), edge_lty = "dashed", edge_size = 1,set_size = 4,
+                              category.names = c("DE" , "DS")) +  scale_fill_distiller(palette = "RdBu") + labs(title = "Dysregulaetd genes by splicing and expression")
+
+
+ggplot2::ggsave(venn_output_file,
+                plot=venn_diag,
+                width=4.5,
+                height=4,
+                device="pdf",
+                dpi=300)
+
+
+## get gene sets relevant to H. sapiens
+hs_msigdb_df <- msigdbr(species = "Homo sapiens")
+pathway_df <- hs_msigdb_df %>%
+  dplyr::filter(gs_cat == "H" | gs_subcat %in% c("CP:KEGG"))
+
+
+## run enrichR to compute and identify significant over-repr pathways
+ora_results <- enricher(
+  gene = intersect$geneSymbol, # A vector of your genes of interest
+  pvalueCutoff = 1, 
+  pAdjustMethod = "BH", 
+  TERM2GENE = dplyr::select(
+    pathway_df,
+    gs_name,
+    human_gene_symbol
+  )
+)
+
+ora_result_df <- data.frame(ora_results@result)
+enrich_plot_func<- enrichplot::dotplot(ora_results) +   
+  theme_Publication() +
+  scale_color_gradient(name = "Adjusted p-value", 
+                       low = "orange", high = "#0C7BDC") +  # Modify color range
+  labs(color = "B-H adj p-value")  # Modify legend title 
+
+ggplot2::ggsave(ora_dotplot_func_path,
+                plot=enrich_plot_func,
+                width=9,
+                height=5,
+                device="pdf",
+                dpi=300)
+
