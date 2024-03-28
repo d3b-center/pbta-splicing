dd80f43842c711023f234177dc2d59fe9c1058c4
Ammar S. Naqvi <naqvia@email.chop.edu>
2024-03-28 16:26:35 -0400

    modified to address comments

--- analyses/CLK1-splicing_correlations/00-subset-CLK1-from-rMATs.R
+++ analyses/CLK1-splicing_correlations/00-subset-CLK1-from-rMATs.R
@@ -8,8 +8,6 @@
 # Load libraries
 suppressPackageStartupMessages({
   library(tidyverse)
-  library(ggplot2)
-  library(vroom)
   library(data.table)
 })
 
@@ -19,12 +17,9 @@ suppressPackageStartupMessages({
 root_dir <- rprojroot::find_root(rprojroot::has_dir(".git"))
 data_dir <- file.path(root_dir, "data")
 analysis_dir <- file.path(root_dir, "analyses", "CLK1-splicing_correlations")
-input_dir <- file.path(analysis_dir, "input")
+results_dir <- file.path(analysis_dir, "results")
 
 # Specify file paths
-rmats_file <- file.path(data_dir, "splice-events-rmats.tsv.gz")
-
-## output file
 rmats_file <- file.path(data_dir, "splice-events-rmats.tsv.gz")
 
 ## Load rmats file
@@ -32,4 +27,4 @@ rmats_df <-  fread(rmats_file) %>%
   # Select CLK1 gene
   filter(geneSymbol=="CLK1") 
 
-write_tsv(rmats_df,file.path(input_dir, "clk1-splice-events-rmats.tsv"))
+write_tsv(rmats_df,file.path(results_dir, "clk1-splice-events-rmats.tsv"))
