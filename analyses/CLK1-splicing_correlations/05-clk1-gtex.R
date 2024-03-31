6d48eac9b52b894976929101b9bac8506103015f
jharenza <jharenza@gmail.com>
2024-03-31 19:06:20 +0000

    add CLk1 exon 4 pHGG vs gtex plot

--- analyses/CLK1-splicing_correlations/05-clk1-gtex.R
+++ analyses/CLK1-splicing_correlations/05-clk1-gtex.R
@@ -10,6 +10,7 @@
 suppressPackageStartupMessages({
   library("tidyverse")
   library("ggplot2")
+  library("data.table")
 })
 
 # Get `magrittr` pipe
@@ -33,74 +34,88 @@ figures_dir <- file.path(root_dir, "figures")
 source(file.path(figures_dir, "theme_for_plots.R"))
 
 ## input files
-hist_file <- file.path(data_dir, "histologies.tsv")
-v15_hist_file <- file.path(data_dir, "v15-histologies.tsv")
-pbta_counts_file <- file.path(data_dir, "gene-counts-rsem-expected_count-collapsed.rds") 
-gtex_counts_file <- file.path(data_dir, "gtex_gene-counts-rsem-expected_count-collapsed.rds")
+all_hist_file <- file.path(data_dir, "histologies.tsv")
+pbta_hist_file <- file.path(data_dir, "histologies-plot-group.tsv")
+pbta_tpm_file <- file.path(data_dir, "rna-isoform-expression-rsem-tpm.rds") 
+gtex_trans_tpm_file <- file.path(data_dir, "GTEx_Analysis_2017-06-05_v8_RSEMv1.3.0_transcript_tpm.gct.gz")
 indep_file <- file.path(data_dir, "independent-specimens.rnaseqpanel.primary.tsv")
 
 ## output file for plot
-gtex_plot_path <- file.path(plots_dir, "CLK1-gtex-hgg.pdf")
+gtex_plot_path <- file.path(plots_dir, "CLK1ex4-gtex-hgg.pdf")
 
 ## load files
 indep_df <- read_tsv(indep_file) %>% 
   dplyr::filter(cohort=='PBTA')
 
-all_hgg_hist <- read_tsv(hist_file, guess_max = 100000) %>% 
-  dplyr::filter(short_histology == "HGAT",
+hist <- read_tsv(all_hist_file, guess_max = 100000)
+
+all_hgg_hist <- read_tsv(pbta_hist_file) %>% 
+  dplyr::filter(plot_group %in% c("Other high-grade glioma", "DIPG or DMG"),
                 Kids_First_Biospecimen_ID %in% 
                   indep_df$Kids_First_Biospecimen_ID)
 
-gtex_hist <- read_tsv(v15_hist_file, guess_max = 100000) %>% 
-  dplyr::filter(cohort == "GTEx")
+gtex_brain <- hist %>% 
+  dplyr::filter(cohort == "GTEx",
+                gtex_group == "Brain")
 
-library(tidyverse)
-
-hgg_counts <- readRDS(pbta_counts_file) %>%
-  select(all_hgg_hist$Kids_First_Biospecimen_ID) %>%
-  rownames_to_column("Gene") %>%
-  filter(Gene == "CLK1") %>%
-  select(-Gene) %>%
+# transcript is ENST00000321356.9 in pbta
+hgg_counts <- readRDS(pbta_tpm_file) %>%
+  select(c(transcript_id, all_hgg_hist$Kids_First_Biospecimen_ID)) %>%
+  filter(transcript_id == "ENST00000321356.9") %>%
+  select(-transcript_id) %>%
   # Transpose and then immediately convert to a data frame or tibble, capturing row names as a column
   {data.frame(t(.))} %>%
   rownames_to_column("Kids_First_Biospecimen_ID") %>%
-  dplyr::rename(CLK1 = `t...`) %>%
-  mutate(plot_group = "pHGG")
+  dplyr::rename(ENST00000321356 = `t...`) %>%
+  left_join(all_hgg_hist[,c("Kids_First_Biospecimen_ID", "plot_group")])
 
-
-gtex_counts <- readRDS(gtex_counts_file) %>%
-  rownames_to_column("Gene") %>%
-  filter(Gene == "CLK1") %>%
-  select(-Gene) %>%
+# transcript is ENST00000321356.8 in gtex
+gtex_clk1ex4_counts <- fread(gtex_trans_tpm_file, skip = 2) %>%
+  select(c(transcript_id, gtex_brain$Kids_First_Biospecimen_ID)) %>%
+  filter(transcript_id == "ENST00000321356.8") %>%
+  select(-transcript_id) %>%
   {data.frame(t(.))} %>%
   rownames_to_column("Kids_First_Biospecimen_ID") %>%
-  dplyr::rename(CLK1 = `t...`) %>%
-  left_join(gtex_hist[,c("Kids_First_Biospecimen_ID", "gtex_group", "gtex_subgroup")]) %>%
-  dplyr::rename(plot_group = gtex_subgroup) %>%
-  filter(gtex_group == "Brain")
+  dplyr::rename(ENST00000321356 = `t...`) %>%
+  left_join(gtex_brain[,c("Kids_First_Biospecimen_ID", "gtex_subgroup")]) %>%
+  dplyr::rename(plot_group = gtex_subgroup)
 
 combined_df <- hgg_counts %>%
-  bind_rows(gtex_counts)
+  bind_rows(gtex_clk1ex4_counts)
 
 # Calculate medians and reorder plot_group levels based on these medians
 combined_df_ordered <- combined_df %>%
   group_by(plot_group) %>%
-  mutate(median_CLK1 = median(CLK1, na.rm = TRUE)) %>%
+  mutate(median_CLK1 = median(ENST00000321356, na.rm = TRUE)) %>%
   ungroup() %>%
   mutate(plot_group = factor(plot_group, levels = unique(plot_group[order(median_CLK1)])))
 
+# color palette
+pal <- combined_df %>%
+  select(plot_group) %>%
+  unique() %>%
+  left_join(unique(all_hgg_hist[,c("plot_group", "plot_group_hex")])) %>%
+  mutate(plot_group_hex = case_when(is.na(plot_group_hex) ~ "lightgrey",
+                                    TRUE ~ plot_group_hex))
+
+colors <- setNames(pal$plot_group_hex, pal$plot_group)
+
+
+
 # plot
-gtex_plot <- ggplot(combined_df_ordered, aes(x = plot_group, y = log2(CLK1))) +
-  geom_boxplot(outlier.shape = NA) + # Remove outliers for cleaner look
-  geom_jitter(width = 0.2, color = "black", alpha = 0.5) + # Add actual data points
+gtex_plot <- ggplot(combined_df_ordered, aes(x = plot_group, y = ENST00000321356)) +
+  geom_boxplot(outlier.shape = NA) + 
+  geom_jitter(aes(fill = plot_group), width = 0.25, size = 2, shape = 21,color = "black") + # Add actual data points
+  scale_fill_manual(values = colors) +
   labs(title = "",
        x = "Group",
-       y = "log2[CLK1 RSEM expected counts]") +
-  theme_Publication() +
+       y = expression(bold(bolditalic("CLK1 ENST00000321356")*" TPM (log"[2]*")"))) +
+       theme_Publication() +
   theme(legend.position = "none", 
-        axis.text.x = element_text(angle = 75, hjust = 1))
+        axis.text.x = element_text(angle = 75, hjust = 1)) +
+  scale_x_discrete(labels = function(x) sapply(x, function(l) str_wrap(l, width = 20))) # Wrap x-axis labels
 
-pdf(gtex_plot_path, height = 8, width = 8, useDingbats = FALSE)
+pdf(gtex_plot_path, height = 6, width = 9, useDingbats = FALSE)
 print(gtex_plot)
 dev.off()
 
