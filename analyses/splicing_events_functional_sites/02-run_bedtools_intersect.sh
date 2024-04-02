2ac73ab2cb4a53ed06fc63453739900d9a26edca
Ammar S. Naqvi <naqvia@email.chop.edu>
2024-04-01 21:36:58 -0400

    made it less redundant

--- analyses/splicing_events_functional_sites/02-run_bedtools_intersect.sh
+++ analyses/splicing_events_functional_sites/02-run_bedtools_intersect.sh
@@ -1,39 +1,22 @@
   #!/bin/sh
 
 ## Cross reference splicing events with Unipro database (*hg38.col.bed)
+#!/bin/sh
 
-bedtools intersect -wo -a "results/splicing_events.SE.total.Other high-grade glioma.pos.bed" -b input/unipMod.hg38.col.bed       |sort -u > results/splicing_events.total.other-HGG.pos.intersectUnipMod.wo.txt
-bedtools intersect -wo -a "results/splicing_events.SE.total.Other high-grade glioma.pos.bed" -b input/unipOther.hg38.col.bed     |sort -u > results/splicing_events.total.other-HGG.pos.intersectUnipOther.wo.txt
-bedtools intersect -wo -a "results/splicing_events.SE.total.Other high-grade glioma.pos.bed" -b input/unipDisulfBond.hg38col.bed |sort -u > results/splicing_events.total.other-HGG.pos.intersectUnipDisulfBond.wo.txt
-bedtools intersect -wo -a "results/splicing_events.SE.total.Other high-grade glioma.pos.bed" -b input/unipLocSignal.hg38.col.bed |sort -u > results/splicing_events.total.other-HGG.pos.intersectUnipLocSignal.wo.txt
-bedtools intersect -wo -a "results/splicing_events.SE.total.Other high-grade glioma.pos.bed" -b input/unipDomain.hg38.col.bed    |sort -u > results/splicing_events.total.other-HGG.pos.intersectUnipDomain.wo.txt
+# Define the input and output directories
+input_dir="input/"
+output_dir="results/"
 
-bedtools intersect -wo -a "results/splicing_events.SE.total.Other high-grade glioma.neg.bed"  -b input/unipMod.hg38.col.bed       |sort -u > results/splicing_events.total.other-HGG.neg.intersectUnipMod.wo.txt
-bedtools intersect -wo -a "results/splicing_events.SE.total.Other high-grade glioma.neg.bed"  -b input/unipOther.hg38.col.bed     |sort -u > results/splicing_events.total.other-HGG.neg.intersectUnipOther.wo.txt
-bedtools intersect -wo -a "results/splicing_events.SE.total.Other high-grade glioma.neg.bed" -b input/unipDisulfBond.hg38col.bed |sort -u > results/splicing_events.total.other-HGG.neg.intersectUnipDisulfBond.wo.txt
-bedtools intersect -wo -a "results/splicing_events.SE.total.Other high-grade glioma.neg.bed" -b input/unipLocSignal.hg38.col.bed |sort -u > results/splicing_events.total.other-HGG.neg.intersectUnipLocSignal.wo.txt
-bedtools intersect -wo -a "results/splicing_events.SE.total.Other high-grade glioma.neg.bed" -b input/unipDomain.hg38.col.bed    |sort -u > results/splicing_events.total.other-HGG.neg.intersectUnipDomain.wo.txt
+# Define the bed files
+bed_files=("unipMod.hg38.col.bed" "unipOther.hg38.col.bed" "unipDisulfBond.hg38col.bed" "unipLocSignal.hg38.col.bed" "unipDomain.hg38.col.bed")
 
-bedtools intersect -wo -a results/splicing_events.SE.total.DMG.pos.bed -b input/unipMod.hg38.col.bed       |sort -u > results/splicing_events.total.DMG.pos.intersectUnipMod.wo.txt
-bedtools intersect -wo -a results/splicing_events.SE.total.DMG.pos.bed -b input/unipOther.hg38.col.bed     |sort -u > results/splicing_events.total.DMG.pos.intersectUnipOther.wo.txt
-bedtools intersect -wo -a results/splicing_events.SE.total.DMG.pos.bed -b input/unipDisulfBond.hg38col.bed |sort -u > results/splicing_events.total.DMG.pos.intersectUnipDisulfBond.wo.txt
-bedtools intersect -wo -a results/splicing_events.SE.total.DMG.pos.bed -b input/unipLocSignal.hg38.col.bed |sort -u > results/splicing_events.total.DMG.pos.intersectUnipLocSignal.wo.txt
-bedtools intersect -wo -a results/splicing_events.SE.total.DMG.pos.bed -b input/unipDomain.hg38.col.bed    |sort -u > results/splicing_events.total.DMG.pos.intersectUnipDomain.wo.txt
+# Define the splicing event files
+splicing_event_files=("splicing_events.SE.total.Other high-grade glioma.pos.bed" "splicing_events.SE.total.Other high-grade glioma.neg.bed" "splicing_events.SE.total.DMG.pos.bed" "splicing_events.SE.total.DMG.neg.bed" "splicing_events.SE.total.pos.bed" "splicing_events.SE.total.neg.bed")
 
-bedtools intersect -wo -a results/splicing_events.SE.total.DMG.neg.bed -b input/unipMod.hg38.col.bed       |sort -u > results/splicing_events.total.DMG.neg.intersectUnipMod.wo.txt
-bedtools intersect -wo -a results/splicing_events.SE.total.DMG.neg.bed -b input/unipOther.hg38.col.bed     |sort -u > results/splicing_events.total.DMG.neg.intersectUnipOther.wo.txt
-bedtools intersect -wo -a results/splicing_events.SE.total.DMG.neg.bed -b input/unipDisulfBond.hg38col.bed |sort -u > results/splicing_events.total.DMG.neg.intersectUnipDisulfBond.wo.txt
-bedtools intersect -wo -a results/splicing_events.SE.total.DMG.neg.bed -b input/unipLocSignal.hg38.col.bed |sort -u > results/splicing_events.total.DMG.neg.intersectUnipLocSignal.wo.txt
-bedtools intersect -wo -a results/splicing_events.SE.total.DMG.neg.bed -b input/unipDomain.hg38.col.bed    |sort -u > results/splicing_events.total.DMG.neg.intersectUnipDomain.wo.txt
-
-bedtools intersect -wo -a results/splicing_events.SE.total.pos.bed -b input/unipMod.hg38.col.bed       |sort -u > results/splicing_events.total.pos.intersectUnipMod.wo.txt
-bedtools intersect -wo -a results/splicing_events.SE.total.pos.bed -b input/unipOther.hg38.col.bed     |sort -u > results/splicing_events.total.pos.intersectUnipOther.wo.txt
-bedtools intersect -wo -a results/splicing_events.SE.total.pos.bed -b input/unipDisulfBond.hg38col.bed |sort -u > results/splicing_events.total.pos.intersectUnipDisulfBond.wo.txt
-bedtools intersect -wo -a results/splicing_events.SE.total.pos.bed -b input/unipLocSignal.hg38.col.bed |sort -u > results/splicing_events.total.pos.intersectUnipLocSignal.wo.txt
-bedtools intersect -wo -a results/splicing_events.SE.total.pos.bed -b input/unipDomain.hg38.col.bed    |sort -u > results/splicing_events.total.pos.intersectUnipDomain.wo.txt
-
-bedtools intersect -wo -a results/splicing_events.SE.total.neg.bed -b input/unipMod.hg38.col.bed       |sort -u > results/splicing_events.total.neg.intersectUnipMod.wo.txt
-bedtools intersect -wo -a results/splicing_events.SE.total.neg.bed -b input/unipOther.hg38.col.bed     |sort -u > results/splicing_events.total.neg.intersectUnipOther.wo.txt
-bedtools intersect -wo -a results/splicing_events.SE.total.neg.bed -b input/unipDisulfBond.hg38col.bed |sort -u > results/splicing_events.total.neg.intersectUnipDisulfBond.wo.txt
-bedtools intersect -wo -a results/splicing_events.SE.total.neg.bed -b input/unipLocSignal.hg38.col.bed |sort -u > results/splicing_events.total.neg.intersectUnipLocSignal.wo.txt
-bedtools intersect -wo -a results/splicing_events.SE.total.neg.bed -b input/unipDomain.hg38.col.bed    |sort -u > results/splicing_events.total.neg.intersectUnipDomain.wo.txt
+# Loop through each splicing event file and bed file combination
+for splicing_file in "${splicing_event_files[@]}"; do
+    for bed_file in "${bed_files[@]}"; do
+        output_file="${output_dir}${splicing_file%.*}.intersect${bed_file%.*}.wo.txt"
+        bedtools intersect -wo -a "${output_dir}${splicing_file}" -b "${input_dir}${bed_file}" | sort -u > "$output_file"
+    done
+done
