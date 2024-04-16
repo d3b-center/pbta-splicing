d16232dbb88ceafe671bfc11102c1d7d89c67ccc
Ammar S. Naqvi <naqvia@email.chop.edu>
2024-04-16 11:57:10 -0400

    add back script (deleted by mistake)

--- analyses/CLK1-splicing_correlations/05-CLK-SRSF-expr-correlations.R
+++ analyses/CLK1-splicing_correlations/05-CLK-SRSF-expr-correlations.R
@@ -50,7 +50,7 @@ all_hgg_bsids <- cohort_df %>%
   filter(plot_group %in% c("DIPG or DMG", "Other high-grade glioma"),
          RNA_library == "stranded",
          cohort == "PBTA",
-      Kids_First_Biospecimen_ID %in% indep_rna_df$Kids_First_Biospecimen_ID) %>%
+         Kids_First_Biospecimen_ID %in% indep_rna_df$Kids_First_Biospecimen_ID) %>%
   select(Kids_First_Biospecimen_ID, RNA_library, CNS_region, match_id, molecular_subtype, plot_group)
 
 CLK1_ex4_rsem <- readRDS(isoform_file) %>%
@@ -103,7 +103,7 @@ region_list <- c("midline", "other", "all")
 
 # Loop through SRSF and CLK gene lists to assess correlation between CLK1 exon4 inclusion and gene expression
 for (goi in names(goi_list)) {
-   # assess correlations in all HGG, DMG, and other HGG separately
+  # assess correlations in all HGG, DMG, and other HGG separately
   for (brain_region in region_list) {
     
     if (brain_region == "all"){
@@ -125,69 +125,69 @@ for (goi in names(goi_list)) {
         pull(Kids_First_Biospecimen_ID)
     }
     
-  # make scatterplot
-   if (goi == "SRSF"){
-     
-          # filter count data for GOI
-     plot_df <- rmats_exp_df %>%
-       dplyr::filter(sample_id %in% bs_id_list,
-                     geneSymbol %in% srsf_list)
-     
-     # create scatterplots between ex4 inc and SRSF/SRPK RNA expr
-     p <-  ggplot(plot_df, aes(x = IncLevel1, y = logExp)) +
-       geom_point() +
-       stat_smooth(method = "lm", 
-                   formula = y ~ x, 
-                   geom = "smooth", 
-                   colour = "red",
-                   fill = "pink",
-                   linetype="dashed") +
-       labs(x = "CLK1 Exon 4 Inclusion (PSI)",
-            y = "RSEM TPM (log2)") + 
-       stat_cor(method = "pearson",
-                label.x = 0, label.y = 8, size = 3) +
-      # ylim(c(7.5, 15)) +
-       facet_wrap(~geneSymbol, nrow = 4) + 
-       theme_Publication()
-     
-     # save plot
-     pdf(file.path(paste(plots_dir, "/", goi, "_exp_vs_CLK1_psi_", brain_region, "_hgg.pdf", sep = "")), width = 8, height = 10)
-     print(p)
-     dev.off()
-     
-   } else {
-     
-     for(i in 2:3){
-     plot_df <- rmats_exp_df %>%
-       dplyr::filter(sample_id %in% bs_id_list,
-                     geneSymbol %in% goi_list[[i]])
-     
-     # create scatterplots between ex4 inclusion and CLK RNA expr
-     p <-  ggplot(plot_df, aes(x = IncLevel1, y = logExp)) +
-       geom_point(colour = "black") +
-       stat_smooth(method = "lm", 
-                   formula = y ~ x, 
-                   geom = "smooth", 
-                   colour = "red",
-                   fill = "pink",
-                   linetype="dashed") +
-       labs(x = "CLK1 Exon 4 Inclusion (PSI)",
-            y = "RSEM TPM (log2)") + 
-       stat_cor(method = "pearson",
-                label.x = 0, label.y = 6, size = 3) +
-      # ylim(c(NA, 15)) +
-       facet_wrap(~geneSymbol, nrow = 5, scales = "free_y") + 
-       theme_Publication()
-     
-     # save plot
-     pdf(file.path(paste(plots_dir, "/", goi, "_exp_vs_CLK1_psi_", brain_region, "_hgg.pdf", sep = "")), width = 4, height = 8)
-     print(p)
-     dev.off()
-     } 
-   }
-   
+    # make scatterplot
+    if (goi == "SRSF"){
+      
+      # filter count data for GOI
+      plot_df <- rmats_exp_df %>%
+        dplyr::filter(sample_id %in% bs_id_list,
+                      geneSymbol %in% srsf_list)
+      
+      # create scatterplots between ex4 inc and SRSF/SRPK RNA expr
+      p <-  ggplot(plot_df, aes(x = IncLevel1, y = logExp)) +
+        geom_point() +
+        stat_smooth(method = "lm", 
+                    formula = y ~ x, 
+                    geom = "smooth", 
+                    colour = "red",
+                    fill = "pink",
+                    linetype="dashed") +
+        labs(x = "CLK1 Exon 4 Inclusion (PSI)",
+             y = "RSEM TPM (log2)") + 
+        stat_cor(method = "pearson",
+                 label.x = 0, label.y = 8, size = 3) +
+        # ylim(c(7.5, 15)) +
+        facet_wrap(~geneSymbol, nrow = 4) + 
+        theme_Publication()
+      
+      # save plot
+      pdf(file.path(paste(plots_dir, "/", goi, "_exp_vs_CLK1_psi_", brain_region, "_hgg.pdf", sep = "")), width = 8, height = 10)
+      print(p)
+      dev.off()
+      
+    } else {
+      
+      for(i in 2:3){
+        plot_df <- rmats_exp_df %>%
+          dplyr::filter(sample_id %in% bs_id_list,
+                        geneSymbol %in% goi_list[[i]])
+        
+        # create scatterplots between ex4 inclusion and CLK RNA expr
+        p <-  ggplot(plot_df, aes(x = IncLevel1, y = logExp)) +
+          geom_point(colour = "black") +
+          stat_smooth(method = "lm", 
+                      formula = y ~ x, 
+                      geom = "smooth", 
+                      colour = "red",
+                      fill = "pink",
+                      linetype="dashed") +
+          labs(x = "CLK1 Exon 4 Inclusion (PSI)",
+               y = "RSEM TPM (log2)") + 
+          stat_cor(method = "pearson",
+                   label.x = 0, label.y = 6, size = 3) +
+          # ylim(c(NA, 15)) +
+          facet_wrap(~geneSymbol, nrow = 5, scales = "free_y") + 
+          theme_Publication()
+        
+        # save plot
+        pdf(file.path(paste(plots_dir, "/", goi, "_exp_vs_CLK1_psi_", brain_region, "_hgg.pdf", sep = "")), width = 4, height = 8)
+        print(p)
+        dev.off()
+      } 
+    }
+    
   }
-   
+  
 }
 
 # plot CLK expr vs SRSF/SRPK expr
@@ -242,8 +242,8 @@ for (clk in clk_list) {
              y = "RSEM TPM (log2)") + 
         stat_cor(method = "pearson",
                  label.x = -4, label.y = 8, size = 3) +
-       # xlim(c(-5,6)) +
-       # ylim(c(NA, 15.5)) +
+        # xlim(c(-5,6)) +
+        # ylim(c(NA, 15.5)) +
         facet_wrap(~geneSymbol, nrow = 3, scales = "free_y") + 
         theme_Publication()
       
@@ -261,13 +261,13 @@ for (clk in clk_list) {
              y = "RSEM TPM (log2)") + 
         stat_cor(method = "pearson",
                  label.x = 0, label.y = 8, size = 3) +
-       # xlim(c(NA,13)) +
-       # ylim(c(NA, 15.5)) +
+        # xlim(c(NA,13)) +
+        # ylim(c(NA, 15.5)) +
         facet_wrap(~geneSymbol, nrow = 3, scales = "free_y") + 
         theme_Publication()
       
     }
-  
+    
     # save plot
     pdf(file.path(paste(plots_dir, "/", clk, "_exp_vs_SRSF_SRPK_exp_", brain_region, "_hgg.pdf", sep = "")), width = 12, height = 8)
     print(p)
@@ -276,4 +276,3 @@ for (clk in clk_list) {
   }
   
 }
-
