405b0169ebee0115a83e72ab9795ad290d99a2b9
Ammar S. Naqvi <naqvia@email.chop.edu>
2024-09-26 15:35:25 -0400

    odd ratio

--- analyses/clustering_analysis/code/09-plot-histology-distr-across-clusters.R
+++ analyses/clustering_analysis/code/09-plot-histology-distr-across-clusters.R
@@ -109,8 +109,14 @@ hist_subset %>%
   scale_fill_manual(name = "Molecular Subtype", values = cols) 
 dev.off()
 
+# write out bs id with cancer group, subtype, and cluster
+histologies_df %>%
+  dplyr::select(Kids_First_Biospecimen_ID, cancer_group, plot_group, molecular_subtype, molecular_subtype_display, cluster_assigned) %>%
+  write_tsv(cluster_membership_tsv)
+
+
 # 
-fusion_info_df <- vroom(file.path("/Users/naqvia/d3b_coding/pbta-splicing/analyses/lgg-clusters/input/lgg-braf-fusion-breakpoint-annotation.tsv"))
+fusion_info_df <- vroom(file.path(input_dir,"lgg-braf-fusion-breakpoint-annotation.tsv"))
 histologies_w_fusion_df <- histologies_df %>%
   left_join(fusion_info_df, by = 'Kids_First_Biospecimen_ID') %>%
   # Replace NA values in 'breakpoint_group' and 'breakpoint_type' with 'non-fusion'
@@ -128,13 +134,85 @@ histologies_w_fusion_df <- histologies_w_fusion_df %>%  group_by(cluster_assigne
               percentage = (count / total_in_cluster) * 100) %>%
               arrange(cluster_assigned, desc(percentage))
 
+pdf(file.path(plots_dir, "cluster_membership-subtypes-LGGs-fusion.pdf"), height = 6, width = 11)
 ggplot(histologies_w_fusion_df, aes(x = factor(cluster_assigned), y = percentage, fill = breakpoint_group)) +
   geom_bar(stat = "identity", position = "fill") +
   scale_y_continuous(labels = scales::percent) +
   labs(x = "Cluster Assigned", y = "Percentage", fill = "Breakpoint Group") +
   theme_Publication()
+dev.off()
 
-# write out bs id with cancer group, subtype, and cluster
-histologies_df %>%
-  dplyr::select(Kids_First_Biospecimen_ID, cancer_group, plot_group, molecular_subtype, molecular_subtype_display, cluster_assigned) %>%
-  write_tsv(cluster_membership_tsv)
+plot_enr <- function(df, var1, var2,
+                     var1_names, var2_names,
+                     padjust = FALSE) {
+  
+  enr <- matrix(0, nrow(unique(df[,var1])),
+                nrow(unique(df[,var2])),
+                dimnames = list(var1_names,
+                                var2_names))
+  pval <- enr
+  ct <- enr
+  
+  # loop through cancer groups to calculate enrichment 
+  for (i in 1:nrow(enr)){
+    no_var1 <- sum(unlist(df[,var1]) == rownames(enr)[i])
+    for (j in 1:ncol(enr)){
+      no_var2 <- sum(unlist(df[,var2]) == colnames(enr)[j] & !is.na(unlist(df[,var2])))
+      no_var1_var2 <- sum(unlist(df[,var1]) == rownames(enr)[i] & unlist(df[,var2]) == colnames(enr)[j])
+      ct[i,j] <- no_var1_var2
+      enr[i,j] <- (no_var1_var2/no_var2)/(no_var1/nrow(df))
+      pval[i,j] <- phyper(no_var1_var2, no_var1, nrow(df) - no_var1, no_var2, lower.tail = F)
+    }
+  }
+  
+  if (padjust == TRUE) {
+    
+    fdr <- t(apply(pval, 1, function(x) p.adjust(x, "fdr")))
+    sig_mat <- ifelse(fdr < 0.05 & enr > 1 & ct > 1, "*", "")
+    
+  } else {
+    
+    sig_mat <- ifelse(pval < 0.05 & enr > 1 & ct > 1, "*", "")
+    
+  }
+  
+  fill_mat <- matrix(glue::glue("{round(enr, 1)}{sig_mat}"), 
+                     nrow(enr), ncol(enr))
+  
+  ct_enr_mat <- matrix(glue::glue("{ct}\n({fill_mat})"),
+                       nrow(ct), ncol(ct))
+  
+  col_fun = colorRamp2(c(0, ceiling(max(enr))), c("white", "orangered"))
+  
+  region_ht <- Heatmap(enr,
+                       name = "Odds ratio",
+                       cluster_rows = F,
+                       cluster_columns = F,
+                       rect_gp = gpar(col = "black", lwd = 2),
+                       col = col_fun,
+                       cell_fun = function(j, i, x, y, width, height, fill) {
+                         grid.text(sprintf("%s", ct_enr_mat[i, j]), x, y, gp = gpar(fontsize = 12))
+                       },
+                       column_names_side = "top",
+                       column_names_rot = 25,
+                       column_names_centered = FALSE)
+  
+  return(region_ht)
+  
+}
+
+group_ht <- plot_enr(histologies_w_fusion_df, 
+                     "cluster_assigned", "breakpoint_group",
+                     var1_names = c("1", "2", "3", "6", "7", "8", "9", "10","11"),
+                     var2_names = c("15:09", "16:09", "16:11", "18:10", "rare", "non-fusion"),
+                     padjust = TRUE)
+
+# plot enrichment results
+pdf(file.path(plot_dir, "breakpoint_group_ancestry_ct_enr_heatmap.pdf"),
+    height = 3, width = 4)
+
+draw(group_ht)
+
+invisible(dev.off())
+
+draw(group_ht)
